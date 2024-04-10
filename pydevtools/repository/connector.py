2e0421a91bd371ab13681e528ca8427e8dd76bad
dzabro <139958066+dzabro@users.noreply.github.com>
2024-04-10 17:52:39 +0400

    Add SQLite connectors (#105)

---
+++ pydevtools/repository/connector.py
@@ -0,0 +1,32 @@
+import sqlite3
+from dataclasses import dataclass
+from functools import cached_property
+from typing import ContextManager
+
+from pydevtools.repository import Connection
+
+
+@dataclass
+class SqliteFileConnector:
+    dsn: str
+
+    def connect(self) -> ContextManager[Connection]:
+        connection = sqlite3.connect(self.dsn)
+        connection.row_factory = sqlite3.Row
+
+        return connection
+
+
+@dataclass
+class SqliteInMemoryConnector:
+    dsn: str = ":memory:"
+
+    def connect(self) -> ContextManager[Connection]:
+        return self._connection
+
+    @cached_property
+    def _connection(self) -> ContextManager[Connection]:
+        connection = sqlite3.connect(self.dsn, check_same_thread=False)
+        connection.row_factory = sqlite3.Row
+
+        return connection
---
+++ tests/test_connector.py
@@ -0,0 +1,22 @@
+from pydevtools.repository import Connector, DatabaseCommand
+from pydevtools.repository.connector import SqliteFileConnector, SqliteInMemoryConnector
+
+DSN = "test.sqlite"
+command = DatabaseCommand("""
+    SELECT 1;
+""")
+
+
+def execute_command(connector: Connector) -> None:
+    with connector.connect() as connection:
+        cursor = connection.cursor()
+        cursor.execute(command.value, command.payload)
+        cursor.close()
+
+
+def test_should_connect_to_file() -> None:
+    execute_command(SqliteFileConnector(DSN))
+
+
+def test_should_connect_to_memory() -> None:
+    execute_command(SqliteInMemoryConnector())
