c2dc55c8c038c8a5203849ce9e2b6fad49f58b35
Mikheil Kenchoshvili <MadViper@users.noreply.github.com>
2024-04-19 10:56:07 +0300

    Introduce generic fluent http class (#126)

--- pydevtools/http/httpx.py
+++ pydevtools/http/httpx.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Any, Iterator, Mapping, Protocol, Self, Type, TypeVar
-from warnings import warn
+from typing import Any, Generic, Iterator, Mapping, Protocol, Self, Type, TypeVar
 
 import httpx
 
+from ..annotation import deprecated
 from .fluent import JsonDict, JsonObject
 from .url import HttpUrl
 
@@ -16,9 +16,7 @@ class Http(Protocol[ResponseT]):  # pragma: no cover
     def with_header(self, key: str, value: str) -> Http[ResponseT]:
         pass
 
-    def post(
-        self, endpoint: str, json: dict[str, Any], headers: dict[str, Any] | None = None
-    ) -> ResponseT:
+    def post(self, endpoint: str, json: dict[str, Any]) -> ResponseT:
         pass
 
     def get(self, endpoint: str, params: dict[str, Any] | None = None) -> ResponseT:
@@ -31,8 +29,9 @@ class Http(Protocol[ResponseT]):  # pragma: no cover
         pass
 
 
+@deprecated("FluentHttpx is deprecated, use FluentHttp instead")
 @dataclass(frozen=True)
-class FluentHttpx:
+class FluentHttpx:  # pragma: no cover
     http: Http[httpx.Response]
 
     def and_header(self, key: str, value: str) -> FluentHttpx:
@@ -41,79 +40,108 @@ class FluentHttpx:
     def with_header(self, key: str, value: str) -> FluentHttpx:
         return FluentHttpx(self.http.with_header(key, value))
 
-    def post(self) -> HttpxPost:
+    def post(self) -> HttpxPost[HttpxResponse]:
+        return HttpxPost(HttpxAdapter(self.http))
+
+    def get(self) -> HttpxGet[HttpxResponse]:
+        return HttpxGet(HttpxAdapter(self.http))
+
+    def patch(self) -> HttpxPatch[HttpxResponse]:
+        return HttpxPatch(HttpxAdapter(self.http))
+
+    def delete(self) -> HttpxDelete[HttpxResponse]:
+        return HttpxDelete(HttpxAdapter(self.http))
+
+
+@dataclass(frozen=True)
+class FluentHttp(Generic[ResponseT]):
+    http: Http[ResponseT]
+
+    def and_header(self, key: str, value: str) -> FluentHttp[ResponseT]:
+        return self.with_header(key, value)
+
+    def with_header(self, key: str, value: str) -> FluentHttp[ResponseT]:
+        return FluentHttp(self.http.with_header(key, value))
+
+    def post(self) -> HttpxPost[ResponseT]:
         return HttpxPost(self.http)
 
-    def get(self) -> HttpxGet:
+    def get(self) -> HttpxGet[ResponseT]:
         return HttpxGet(self.http)
 
-    def patch(self) -> HttpxPatch:
+    def patch(self) -> HttpxPatch[ResponseT]:
         return HttpxPatch(self.http)
 
-    def delete(self) -> HttpxDelete:
+    def delete(self) -> HttpxDelete[ResponseT]:
         return HttpxDelete(self.http)
 
 
 @dataclass(frozen=True)
-class HttpxPost:
-    http: Http[httpx.Response]
+class HttpxPost(Generic[ResponseT]):
+    http: Http[ResponseT]
 
     json: JsonDict = field(default_factory=dict)
 
-    def and_json(self, value: JsonDict) -> HttpxPost:  # pragma: no cover
+    def and_json(self, value: JsonDict) -> HttpxPost[ResponseT]:  # pragma: no cover
         return self.with_json(value)
 
-    def with_json(self, value: JsonDict) -> HttpxPost:
+    def with_json(self, value: JsonDict) -> HttpxPost[ResponseT]:
         return HttpxPost(self.http, json=value)
 
-    def and_header(self, key: str, value: str) -> HttpxPost:  # pragma: no cover
+    def and_header(
+        self, key: str, value: str
+    ) -> HttpxPost[ResponseT]:  # pragma: no cover
         return self.with_header(key, value)
 
-    def with_header(self, key: str, value: str) -> HttpxPost:  # pragma: no cover
-        warn(
-            "Method 'HttpxPost.with_header' is deprecated, "
-            "use 'FluentHttp.with_header' instead"
-        )
+    @deprecated(
+        """
+        Method 'HttpxPost.with_header' is deprecated,
+        use 'FluentHttp.with_header' instead.
+        """
+    )
+    def with_header(
+        self, key: str, value: str
+    ) -> HttpxPost[ResponseT]:  # pragma: no cover
         return HttpxPost(self.http.with_header(key, value))
 
-    def on_endpoint(self, value: str) -> HttpxResponse:
-        return HttpxResponse(self.http.post(value, json=self.json))
+    def on_endpoint(self, value: str) -> ResponseT:
+        return self.http.post(value, json=self.json)
 
 
 @dataclass(frozen=True)
-class HttpxGet:
-    http: Http[httpx.Response]
+class HttpxGet(Generic[ResponseT]):
+    http: Http[ResponseT]
 
     params: dict[str, Any] = field(default_factory=dict)
 
-    def with_params(self, **params: Any) -> HttpxGet:
+    def with_params(self, **params: Any) -> HttpxGet[ResponseT]:
         self.params.update(params)
 
         return self
 
-    def on_endpoint(self, value: str) -> HttpxResponse:
-        return HttpxResponse(self.http.get(value, params=self.params))
+    def on_endpoint(self, value: str) -> ResponseT:
+        return self.http.get(value, params=self.params)
 
 
 @dataclass(frozen=True)
-class HttpxPatch:
-    http: Http[httpx.Response]
+class HttpxPatch(Generic[ResponseT]):
+    http: Http[ResponseT]
 
     json: JsonDict = field(default_factory=dict)
 
-    def with_json(self, value: JsonDict) -> HttpxPatch:
+    def with_json(self, value: JsonDict) -> HttpxPatch[ResponseT]:
         return HttpxPatch(self.http, json=value)
 
-    def on_endpoint(self, value: str) -> HttpxResponse:
-        return HttpxResponse(self.http.patch(value, json=self.json))
+    def on_endpoint(self, value: str) -> ResponseT:
+        return self.http.patch(value, json=self.json)
 
 
 @dataclass(frozen=True)
-class HttpxDelete:
-    http: Http[httpx.Response]
+class HttpxDelete(Generic[ResponseT]):
+    http: Http[ResponseT]
 
-    def on_endpoint(self, value: str) -> HttpxResponse:
-        return HttpxResponse(self.http.delete(value))
+    def on_endpoint(self, value: str) -> ResponseT:
+        return self.http.delete(value)
 
 
 @dataclass(frozen=True)
@@ -154,15 +182,8 @@ class Httpx:
     def with_header(self, key: str, value: str) -> Http[httpx.Response]:
         return Httpx(self.url, self.config.with_header(key, value))
 
-    def post(
-        self, endpoint: str, json: dict[str, Any], headers: dict[str, Any] | None = None
-    ) -> httpx.Response:
-        if headers:
-            warn("Parameter 'headers' is deprecated, use '.with_header' before .post()")
-
-        return httpx.post(
-            self.url + endpoint, json=json, **self.config.add_headers(headers or {})
-        )
+    def post(self, endpoint: str, json: dict[str, Any]) -> httpx.Response:
+        return httpx.post(self.url + endpoint, json=json, **self.config)
 
     def get(
         self, endpoint: str, params: dict[str, Any] | None = None
@@ -177,6 +198,26 @@ class Httpx:
 
 
 @dataclass(frozen=True)
+class HttpxAdapter:  # pragma: no cover
+    http: Http[httpx.Response]
+
+    def with_header(self, key: str, value: str) -> Http[HttpxResponse]:
+        return HttpxAdapter(self.http.with_header(key, value))
+
+    def post(self, endpoint: str, json: dict[str, Any]) -> HttpxResponse:
+        return HttpxResponse(self.http.post(endpoint, json=json))
+
+    def get(self, endpoint: str, params: dict[str, Any] | None = None) -> HttpxResponse:
+        return HttpxResponse(self.http.get(endpoint, params=params))
+
+    def patch(self, endpoint: str, json: dict[str, Any]) -> HttpxResponse:
+        return HttpxResponse(self.http.patch(endpoint, json=json))
+
+    def delete(self, endpoint: str) -> HttpxResponse:
+        return HttpxResponse(self.http.delete(endpoint))
+
+
+@dataclass(frozen=True)
 class HttpxConfig(Mapping[str, Any]):
     timeout_s: int = 5
     headers: dict[str, str] = field(default_factory=dict)
@@ -186,16 +227,13 @@ class HttpxConfig(Mapping[str, Any]):
 
         return self.add_headers({key: value})
 
+    @deprecated(
+        """
+        The 'with_user_agent(value)' method is deprecated.
+        Please use 'with_header("user-agent", value)' instead.
+        """
+    )
     def with_user_agent(self, value: str) -> HttpxConfig:  # pragma: no cover
-        warn(
-            (
-                """
-                The 'with_user_agent(value)' method is deprecated.
-                Please use 'with_header("user-agent", value)' instead.
-                """
-            )
-        )
-
         return self.with_header(key="user-agent", value=value)
 
     def as_dict(self) -> dict[str, Any]:
--- tests/http/test_fluent_httpx.py
+++ tests/http/test_fluent_httpx.py
@@ -1,12 +1,11 @@
-from pydevtools.http import FluentHttpx
 from pydevtools.http.fake import FakeHttp
-from pydevtools.http.httpx import HttpxResponse
+from pydevtools.http.httpx import FluentHttp
 
 
 def test_should_attach_headers() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).with_header("Harry", "Potter").and_header("Ronald", "Weasley")
+    FluentHttp(http).with_header("Harry", "Potter").and_header("Ronald", "Weasley")
 
     assert http.headers == {"Harry": "Potter", "Ronald": "Weasley"}
 
@@ -14,15 +13,15 @@ def test_should_attach_headers() -> None:
 def test_should_form_post_response() -> None:
     http = FakeHttp()
 
-    response = FluentHttpx(http).post().on_endpoint("/post")
+    response = FluentHttp(http).post().on_endpoint("/post")
 
-    assert response == HttpxResponse(http.response)
+    assert response == http.response
 
 
 def test_should_post_with_defaults() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).post().on_endpoint("/post")
+    FluentHttp(http).post().on_endpoint("/post")
 
     http.request.assert_post().with_json({}).on_endpoint("/post")
 
@@ -30,7 +29,7 @@ def test_should_post_with_defaults() -> None:
 def test_should_post_with_json() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).post().with_json({"Harry": "Potter"}).on_endpoint("/post")
+    FluentHttp(http).post().with_json({"Harry": "Potter"}).on_endpoint("/post")
 
     http.request.assert_post().with_json({"Harry": "Potter"}).on_endpoint("/post")
 
@@ -38,15 +37,15 @@ def test_should_post_with_json() -> None:
 def test_should_form_get_response() -> None:
     http = FakeHttp()
 
-    response = FluentHttpx(http).get().on_endpoint("/get")
+    response = FluentHttp(http).get().on_endpoint("/get")
 
-    assert response == HttpxResponse(http.response)
+    assert response == http.response
 
 
 def test_should_get_with_defaults() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).get().on_endpoint("/get")
+    FluentHttp(http).get().on_endpoint("/get")
 
     http.request.assert_get().with_params({}).on_endpoint("/get")
 
@@ -54,7 +53,7 @@ def test_should_get_with_defaults() -> None:
 def test_should_get_with_params() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).get().with_params(color="yellow").on_endpoint("/get")
+    FluentHttp(http).get().with_params(color="yellow").on_endpoint("/get")
 
     http.request.assert_get().with_params({"color": "yellow"}).on_endpoint("/get")
 
@@ -62,15 +61,15 @@ def test_should_get_with_params() -> None:
 def test_should_form_patch_response() -> None:
     http = FakeHttp()
 
-    response = FluentHttpx(http).patch().on_endpoint("/patch")
+    response = FluentHttp(http).patch().on_endpoint("/patch")
 
-    assert response == HttpxResponse(http.response)
+    assert response == http.response
 
 
 def test_should_patch_with_defaults() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).patch().on_endpoint("/patch")
+    FluentHttp(http).patch().on_endpoint("/patch")
 
     http.request.assert_patch().with_json({}).on_endpoint("/patch")
 
@@ -78,7 +77,7 @@ def test_should_patch_with_defaults() -> None:
 def test_should_patch_with_json() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).patch().with_json({"Harry": "Potter"}).on_endpoint("/patch")
+    FluentHttp(http).patch().with_json({"Harry": "Potter"}).on_endpoint("/patch")
 
     http.request.assert_patch().with_json({"Harry": "Potter"}).on_endpoint("/patch")
 
@@ -86,14 +85,14 @@ def test_should_patch_with_json() -> None:
 def test_should_form_delete_response() -> None:
     http = FakeHttp()
 
-    response = FluentHttpx(http).delete().on_endpoint("/delete")
+    response = FluentHttp(http).delete().on_endpoint("/delete")
 
-    assert response == HttpxResponse(http.response)
+    assert response == http.response
 
 
 def test_should_delete_with_defaults() -> None:
     http = FakeHttp()
 
-    FluentHttpx(http).delete().on_endpoint("/delete")
+    FluentHttp(http).delete().on_endpoint("/delete")
 
     http.request.assert_delete().on_endpoint("/delete")
