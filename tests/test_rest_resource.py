303cfcf31106e44e50510f3229d407cf5c48377d
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-05-30 14:52:57 +0400

    Split tests

--- tests/test_rest_resource.py
+++ tests/test_rest_resource.py
@@ -61,23 +61,32 @@ def test_should_not_list_anything_when_none_exist(resource: RestResource) -> Non
     resource.read_all().ensure().success().with_code(200).and_collection([])
 
 
-def test_should_read_with_params(resource: RestResource) -> None:
+@pytest.mark.skip("WIP")
+def test_should_read_many(resource: RestResource) -> None:
     apple_one = fake.apple()
-    apple_two = fake.apple().with_a(color=apple_one.value_of("color").to(str))
+    apple_two = fake.apple()
+
+    resource.create_many().from_data(apple_one).and_data(apple_two).unpack_many()
+
+    (
+        resource.read_all()
+        .with_params(color=apple_one.value_of("color").to(str))
+        .ensure()
+        .success()
+        .with_code(200)
+        .and_collection([apple_one])
+    )
+
+
+def test_should_read_all(resource: RestResource) -> None:
+    apple_one = fake.apple()
+    apple_two = fake.apple()
 
     apples = (
         resource.create_many().from_data(apple_one).and_data(apple_two).unpack_many()
     )
-    color = list(apples)[0].value_of("color").to(str)
 
-    (
-        resource.read_all()
-        .with_params(color=color)
-        .ensure()
-        .success()
-        .with_code(200)
-        .and_collection(list(apples))
-    )
+    resource.read_all().ensure().success().with_code(200).and_collection(list(apples))
 
 
 def test_should_create(resource: RestResource) -> None:
