ba293a2be3197ea2ce5cf2230c75adb3c0fd22e5
dzabro <dzabiradzedata@gmail.com>
2024-06-10 16:14:22 +0400

    Add update_many endpoint to router

--- tests/sample_api.py
+++ tests/sample_api.py
@@ -73,6 +73,7 @@ def setup() -> FastAPI:
                     .with_read_one_endpoint()
                     .with_read_all_endpoint()
                     .with_update_one_endpoint()
+                    .with_update_many_endpoint()
                     .build()
                 )
             )
