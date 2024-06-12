a8e374565771fb7991c170f7b7707007270ecfe0
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-06-12 23:34:36 +0400

    Implement nested dataclass formatting

--- apexdevkit/formatter.py
+++ apexdevkit/formatter.py
@@ -1,10 +1,10 @@
-from dataclasses import asdict, dataclass
-from typing import Any, Generic, Protocol, TypeVar
+from dataclasses import asdict, dataclass, field
+from typing import Any, Generic, Protocol, Self, TypeVar
 
 ItemT = TypeVar("ItemT")
 
 
-class Formatter(Protocol[ItemT]):
+class Formatter(Protocol[ItemT]):  # pragma: no cover
     def load(self, raw: dict[str, Any]) -> ItemT:
         pass
 
@@ -15,8 +15,20 @@ class Formatter(Protocol[ItemT]):
 @dataclass
 class DataclassFormatter(Generic[ItemT]):
     resource: type[ItemT]
+    sub_formatters: dict[str, Formatter[Any]] = field(default_factory=dict)
+
+    def and_nested(self, **formatters: Formatter[Any]) -> Self:
+        return self.with_nested(**formatters)
+
+    def with_nested(self, **formatters: Formatter[Any]) -> Self:
+        self.sub_formatters.update(formatters)
+
+        return self
 
     def load(self, raw: dict[str, Any]) -> ItemT:
+        for key, formatter in self.sub_formatters.items():
+            raw[key] = formatter.load(raw.pop(key))
+
         return self.resource(**raw)
 
     def dump(self, item: ItemT) -> dict[str, Any]:
