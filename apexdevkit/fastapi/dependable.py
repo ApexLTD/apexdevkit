779b04207eff7e9037ed9139b4807ad847817e4b
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-06-20 16:06:51 +0400

    Ignore dependable injecction in coverage

--- apexdevkit/fastapi/dependable.py
+++ apexdevkit/fastapi/dependable.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from fastapi.requests import Request
 
 
-def inject(dependency: str) -> Any:
+def inject(dependency: str) -> Any:  # pragma: no cover
     def get(request: Request) -> Any:
         return getattr(request.app.state, dependency)
 
