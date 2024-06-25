9dac470298d3bbc7d96fc3169776ba866ed4193d
Tamo <tamar.gvinepadze2014@ens.tsu.edu.ge>
2024-06-25 18:04:19 +0400

    Remove dead code

--- tests/sample_api.py
+++ tests/sample_api.py
@@ -5,10 +5,7 @@ from enum import Enum
 from typing import Any
 from uuid import uuid4
 
-from fastapi import FastAPI
-
-from apexdevkit.fastapi import FastApiBuilder
-from apexdevkit.fastapi.router import RestfulRouter, RestfulServiceBuilder
+from apexdevkit.fastapi.router import RestfulServiceBuilder
 from apexdevkit.fastapi.schema import SchemaFields
 from apexdevkit.fastapi.service import (
     RawCollection,
@@ -17,7 +14,6 @@ from apexdevkit.fastapi.service import (
     RestfulService,
 )
 from apexdevkit.http import JsonDict
-from apexdevkit.testing import RestfulName
 
 
 @dataclass
@@ -90,39 +86,6 @@ class FakeServiceBuilder(RestfulServiceBuilder):
 
     def build(self) -> RestfulService:
         return FakeService(self.data, self.error)
-
-
-def setup(always_return: JsonDict, error: Exception | None = None) -> FastAPI:
-    infra = FakeServiceBuilder().with_exception(error).always_return(always_return)
-
-    return (
-        FastApiBuilder()
-        .with_title("Apple API")
-        .with_version("1.0.0")
-        .with_description("Sample API for unit testing various testing routines")
-        .with_route(
-            apples=RestfulRouter()
-            .with_name(RestfulName("apple"))
-            .with_fields(AppleFields())
-            .with_infra(infra)
-            .with_sub_resource(
-                prices=(
-                    RestfulRouter()
-                    .with_name(RestfulName("price"))
-                    .with_fields(PriceFields())
-                    .with_parent("apple")
-                    .with_infra(infra)
-                    .with_delete_one_endpoint()
-                    .build()
-                )
-            )
-            .default()
-            .with_replace_one_endpoint()
-            .with_replace_many_endpoint()
-            .build()
-        )
-        .build()
-    )
 
 
 class Color(Enum):
