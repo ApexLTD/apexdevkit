c9f15b66c3dbdc2bcf3e01744bed2f9a39e06a1f
Tamo <tamar.gvinepadze2014@ens.tsu.edu.ge>
2024-06-25 17:38:10 +0400

    Introduce module

---
+++ tests/resource/setup.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+from apexdevkit.fastapi import FastApiBuilder
+from apexdevkit.fastapi.router import RestfulRouter
+from apexdevkit.testing import RestfulName
+from tests.sample_api import FakeServiceBuilder, AppleFields, PriceFields
+
+
+def setup(infra: FakeServiceBuilder) -> FastAPI:
+    return (
+        FastApiBuilder()
+        .with_title("Apple API")
+        .with_version("1.0.0")
+        .with_description("Sample API for unit testing various testing routines")
+        .with_route(
+            apples=RestfulRouter()
+            .with_name(RestfulName("apple"))
+            .with_fields(AppleFields())
+            .with_infra(infra)
+            .with_sub_resource(
+                prices=(
+                    RestfulRouter()
+                    .with_name(RestfulName("price"))
+                    .with_fields(PriceFields())
+                    .with_parent("apple")
+                    .with_infra(infra)
+                    .with_delete_one_endpoint()
+                    .build()
+                )
+            )
+            .default()
+            .with_replace_one_endpoint()
+            .with_replace_many_endpoint()
+            .build()
+        )
+        .build()
+    )
