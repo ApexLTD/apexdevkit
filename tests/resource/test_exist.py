7a21977501709c7bca351a7edcaf0540c69d1304
Tamo <tamar.gvinepadze2014@ens.tsu.edu.ge>
2024-06-25 17:52:10 +0400

    Separate exist tests

---
+++ tests/resource/test_exist.py
@@ -0,0 +1,50 @@
+import pytest
+from starlette.testclient import TestClient
+
+from apexdevkit.error import ExistsError
+from apexdevkit.http import JsonDict
+from apexdevkit.testing import RestCollection, RestfulName, RestResource
+from tests.resource.setup import setup
+from tests.sample_api import FakeServiceBuilder
+from tests.test_rest_resource import FakeApple
+
+
+@pytest.fixture
+def apple() -> JsonDict:
+    return FakeApple().json()
+
+
+@pytest.fixture
+def infra(apple: JsonDict) -> FakeServiceBuilder:
+    return FakeServiceBuilder().with_exception(ExistsError()).always_return(apple)
+
+
+@pytest.fixture
+def exists_error_resource(infra: FakeServiceBuilder) -> RestResource:
+    return RestCollection(TestClient(setup(infra)), RestfulName("apple"))
+
+
+def test_should_not_create_existing(
+    apple: JsonDict, exists_error_resource: RestResource
+) -> None:
+    (
+        exists_error_resource.create_one()
+        .from_data(apple)
+        .ensure()
+        .fail()
+        .with_code(409)
+        .and_message("An item<Apple> with the  already exists.")
+    )
+
+
+def test_should_not_create_many_existing(
+    apple: JsonDict, exists_error_resource: RestResource
+) -> None:
+    (
+        exists_error_resource.create_many()
+        .from_data(apple)
+        .ensure()
+        .fail()
+        .with_code(409)
+        .and_message("An item<Apple> with the  already exists.")
+    )
