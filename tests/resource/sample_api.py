4e60ff67083a46cc2d3acda5880b3d3f1f98dd08
Tamo <tamar.gvinepadze2014@ens.tsu.edu.ge>
2024-06-26 11:57:13 +0400

    Refactor tests

--- tests/resource/sample_api.py
+++ tests/resource/sample_api.py
@@ -120,7 +120,7 @@ class Apple:
 
 class AppleFields(SchemaFields):
     def readable(self) -> JsonDict:
-        return JsonDict().with_a(id=str).and_a(name=Name).and_a(color=Color)
+        return JsonDict().with_a(id=str).and_a(name=Name).and_a(color=str)
 
     def editable(self) -> JsonDict:
         return JsonDict().with_a(name=Name)
--- tests/resource/test_success.py
+++ tests/resource/test_success.py
@@ -5,7 +5,6 @@ from uuid import uuid4
 import pytest
 from fastapi.testclient import TestClient
 
-from apexdevkit.fastapi.router import RestfulServiceBuilder
 from apexdevkit.http import JsonDict
 from apexdevkit.testing import RestCollection, RestfulName, RestResource
 from tests.resource.sample_api import SuccessfulService
@@ -18,19 +17,21 @@ def apple() -> JsonDict:
 
 
 @pytest.fixture
-def infra(apple: JsonDict) -> RestfulServiceBuilder:
+def infra(apple: JsonDict) -> SuccessfulService:
     return SuccessfulService(always_return=apple)
 
 
 @pytest.fixture
-def resource(infra: RestfulServiceBuilder) -> RestResource:
+def resource(infra: SuccessfulService) -> RestResource:
     return RestCollection(
         name=RestfulName("apple"),
         http=TestClient(setup(infra)),
     )
 
 
-def test_should_create(apple: JsonDict, resource: RestResource) -> None:
+def test_should_create(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     (
         resource.create_one()
         .from_data(apple)
@@ -39,9 +40,12 @@ def test_should_create(apple: JsonDict, resource: RestResource) -> None:
         .with_code(201)
         .and_item(apple)
     )
+    assert infra.called_with == apple.drop("id")
 
 
-def test_should_create_many(apple: JsonDict, resource: RestResource) -> None:
+def test_should_create_many(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     (
         resource.create_many()
         .from_data(apple)
@@ -50,9 +54,12 @@ def test_should_create_many(apple: JsonDict, resource: RestResource) -> None:
         .with_code(201)
         .and_collection([apple])
     )
+    assert infra.called_with == [apple.drop("id")]
 
 
-def test_should_read_one(apple: JsonDict, resource: RestResource) -> None:
+def test_should_read_one(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     (
         resource.read_one()
         .with_id(apple["id"])
@@ -61,13 +68,19 @@ def test_should_read_one(apple: JsonDict, resource: RestResource) -> None:
         .with_code(200)
         .with_item(apple)
     )
+    assert infra.called_with == apple["id"]
 
 
-def test_should_read_all(apple: JsonDict, resource: RestResource) -> None:
+def test_should_read_all(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     resource.read_all().ensure().success().with_code(200).and_collection([apple])
+    assert infra.called_with is None
 
 
-def test_should_update_one(apple: JsonDict, resource: RestResource) -> None:
+def test_should_update_one(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     (
         resource.update_one()
         .with_id(apple["id"])
@@ -76,22 +89,35 @@ def test_should_update_one(apple: JsonDict, resource: RestResource) -> None:
         .success()
         .with_code(200)
     )
+    assert infra.called_with == (apple["id"], apple.drop("id").drop("color"))
 
 
-def test_should_update_many(apple: JsonDict, resource: RestResource) -> None:
+def test_should_update_many(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     resource.update_many().from_data(apple).ensure().success().with_code(200)
+    assert infra.called_with == [apple.drop("color")]
 
 
-def test_should_replace_one(apple: JsonDict, resource: RestResource) -> None:
+def test_should_replace_one(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     resource.replace_one().from_data(apple).ensure().success().with_code(200)
+    assert infra.called_with == apple
 
 
-def test_should_replace_many(apple: JsonDict, resource: RestResource) -> None:
+def test_should_replace_many(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     resource.replace_many().from_data(apple).ensure().success().with_code(200)
+    assert infra.called_with == [apple]
 
 
-def test_should_delete_one(apple: JsonDict, resource: RestResource) -> None:
+def test_should_delete_one(
+    apple: JsonDict, infra: SuccessfulService, resource: RestResource
+) -> None:
     (resource.delete_one().with_id(apple["id"]).ensure().success().with_code(200))
+    assert infra.called_with == apple["id"]
 
 
 def test_should_sub_resource(apple: JsonDict, resource: RestCollection) -> None:
