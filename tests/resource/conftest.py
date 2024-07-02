aef44b822b97bc0356741a01859129bcfaa46466
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-07-02 12:39:49 +0400

    Wrap TestClient into Httpx

--- tests/resource/conftest.py
+++ tests/resource/conftest.py
@@ -2,10 +2,14 @@ import pytest
 from starlette.testclient import TestClient
 
 from apexdevkit.fastapi import RestfulServiceBuilder
+from apexdevkit.http import Httpx
 from apexdevkit.testing import RestCollection, RestfulName, RestResource
 from tests.resource.setup import setup
 
 
 @pytest.fixture
 def resource(service: RestfulServiceBuilder) -> RestResource:
-    return RestCollection(name=RestfulName("apple"), http=TestClient(setup(service)))
+    return RestCollection(
+        name=RestfulName("apple"),
+        http=Httpx(TestClient(setup(service))),
+    )
