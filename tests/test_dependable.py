1a6ff0b384ed5b90c53dd918d6272f127efbab82
Tamo <tamar.gvinepadze2014@ens.tsu.edu.ge>
2024-07-03 12:33:55 +0400

    Test parent dependency

--- tests/test_dependable.py
+++ tests/test_dependable.py
@@ -10,13 +10,14 @@ from apexdevkit.fastapi import (
 )
 from apexdevkit.fastapi.dependable import (
     InfraDependency,
+    ParentDependency,
     ServiceDependency,
     UserDependency,
 )
 from apexdevkit.fastapi.router import RestfulRouter
 from apexdevkit.http import Httpx
 from apexdevkit.testing import RestCollection, RestfulName, RestResource
-from tests.resource.sample_api import AppleFields
+from tests.resource.sample_api import AppleFields, PriceFields
 
 
 def resource_with_dependency(dependency: ServiceDependency) -> RestResource:
@@ -38,6 +39,31 @@ def resource_with_dependency(dependency: ServiceDependency) -> RestResource:
     )
 
 
+def parent_resource(dependency: ServiceDependency) -> RestCollection:
+    return RestCollection(
+        name=RestfulName("apple"),
+        http=Httpx(
+            TestClient(
+                FastApiBuilder()
+                .with_route(
+                    apples=RestfulRouter()
+                    .with_name(RestfulName("apple"))
+                    .with_fields(AppleFields())
+                    .with_sub_resource(
+                        prices=RestfulRouter()
+                        .with_name(RestfulName("price"))
+                        .with_fields(PriceFields())
+                        .default(dependency)
+                        .build()
+                    )
+                    .build()
+                )
+                .build()
+            )
+        ),
+    )
+
+
 @pytest.fixture
 def user() -> Any:
     return "A"
@@ -51,6 +77,21 @@ def extract_user(user: Any) -> Callable[..., Any]:
     return _
 
 
+@pytest.fixture
+def identifier() -> str:
+    return "id"
+
+
+@pytest.fixture
+def parent() -> RestfulName:
+    return RestfulName("apple")
+
+
+@pytest.fixture
+def child() -> RestfulName:
+    return RestfulName("price")
+
+
 def test_should_build_dependable_with_user(
     user: Any, extract_user: Callable[..., Any]
 ) -> None:
@@ -61,3 +102,17 @@ def test_should_build_dependable_with_user(
 
     infra.with_user.assert_called_once_with(user)
     infra.with_user().build.assert_called_once()
+
+
+def test_should_build_dependable_with_parent(
+    identifier: str, parent: RestfulName, child: RestfulName
+) -> None:
+    infra = MagicMock(spec=RestfulServiceBuilder)
+    dependency = ServiceDependency(ParentDependency(parent, InfraDependency(infra)))
+
+    parent_resource(dependency).sub_resource(identifier).sub_resource(
+        child.singular
+    ).read_all().ensure()
+
+    infra.with_parent.assert_called_once_with(identifier)
+    infra.with_parent().build.assert_called_once()
