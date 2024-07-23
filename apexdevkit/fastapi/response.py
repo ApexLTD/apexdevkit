d16e9dff2c3318cbbfd667bf9de2ae6477abac0d
Mikheil Kenchoshvili <MadViper@users.noreply.github.com>
2024-07-23 14:02:47 +0400

    Remove unused code (#94)

--- apexdevkit/fastapi/response.py
+++ apexdevkit/fastapi/response.py
@@ -3,94 +3,35 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any, Iterable
 
-from fastapi import status
-from fastapi.responses import JSONResponse
-
 from apexdevkit.error import DoesNotExistError, ExistsError, ForbiddenError
 from apexdevkit.testing import RestfulName
-
-
-class SuccessResponse(dict[str, Any]):
-    def __init__(self, status_code: int, **kwargs: Any):
-        super().__init__(
-            {
-                "status": "success",
-                "code": status_code,
-                "data": {**kwargs},
-            }
-        )
-
-
-class ResourceFound(SuccessResponse):
-    def __init__(self, **kwargs: Any):
-        super().__init__(status_code=status.HTTP_200_OK, **kwargs)
-
-
-class ResourceCreated(SuccessResponse):
-    def __init__(self, **kwargs: Any):
-        super().__init__(status_code=status.HTTP_201_CREATED, **kwargs)
-
-
-class ErrorResponse(JSONResponse):
-    def __init__(self, status_code: int, message: str, **kwargs: Any):
-        content = {
-            "status": "fail",
-            "code": status_code,
-            "error": {"message": message},
-        }
-
-        if kwargs:
-            content["data"] = {**kwargs}
-
-        super().__init__(status_code=status_code, content=content)
-
-
-class BadRequest(ErrorResponse):
-    def __init__(self, message: str, **kwargs: Any) -> None:
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            message=message,
-            **kwargs,
-        )
-
-
-class ResourceNotFound(ErrorResponse):
-    def __init__(self, message: str):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, message=message)
-
-
-class ResourceExists(ErrorResponse):
-    def __init__(self, message: str, **kwargs: Any):
-        super().__init__(
-            status_code=status.HTTP_409_CONFLICT,
-            message=message,
-            **kwargs,
-        )
 
 
 @dataclass
 class RestfulResponse:
     name: RestfulName
 
-    def _response(self, code: int, data: Any, error: str = "") -> dict[str, Any]:
-        content: dict[str, Any] = {"code": code, "status": "success"}
-
-        if error:
-            content["status"] = "fail"
-            content["error"] = {"message": error}
-
-        match data:
-            case None:
-                content["data"] = {}
-            case list():
-                content["data"] = {self.name.plural: data, "count": len(data)}
-            case _:
-                content["data"] = {self.name.singular: data}
-
-        return content
-
     def ok(self) -> dict[str, Any]:
         return self._response(200, data=None)
+
+    def found_one(self, item: Any) -> dict[str, Any]:
+        return self._response(200, item)
+
+    def found_many(self, items: list[Any]) -> dict[str, Any]:
+        return self._response(200, items)
+
+    def created_one(self, item: Any) -> dict[str, Any]:
+        return self._response(201, item)
+
+    def created_many(self, items: Iterable[Any]) -> dict[str, Any]:
+        return self._response(201, list(items))
+
+    def forbidden(self, e: ForbiddenError) -> dict[str, Any]:
+        return self._response(
+            403,
+            data={"id": str(e.id)},
+            error=e.message,
+        )
 
     def not_found(self, e: DoesNotExistError) -> dict[str, Any]:
         name = self.name.singular.capitalize()
@@ -110,21 +51,19 @@ class RestfulResponse:
             error=f"An item<{name}> with the {e} already exists.",
         )
 
-    def forbidden(self, e: ForbiddenError) -> dict[str, Any]:
-        return self._response(
-            403,
-            data={"id": str(e.id)},
-            error=e.message,
-        )
+    def _response(self, code: int, data: Any, error: str = "") -> dict[str, Any]:
+        content: dict[str, Any] = {"code": code, "status": "success"}
 
-    def created_one(self, item: Any) -> dict[str, Any]:
-        return self._response(201, item)
+        if error:
+            content["status"] = "fail"
+            content["error"] = {"message": error}
 
-    def created_many(self, items: Iterable[Any]) -> dict[str, Any]:
-        return self._response(201, list(items))
+        match data:
+            case None:
+                content["data"] = {}
+            case list():
+                content["data"] = {self.name.plural: data, "count": len(data)}
+            case _:
+                content["data"] = {self.name.singular: data}
 
-    def found_one(self, item: Any) -> dict[str, Any]:
-        return self._response(200, item)
-
-    def found_many(self, items: list[Any]) -> dict[str, Any]:
-        return self._response(200, items)
+        return content
