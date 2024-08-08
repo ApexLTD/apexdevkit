b722343e84740e200dec90edc3b0d953557a371f
tkokhreidze <t.kokhreidze@apex.ge>
2024-08-08 13:05:34 +0400

    Rearrange

--- apexdevkit/repository/sqlite.py
+++ apexdevkit/repository/sqlite.py
@@ -15,24 +15,6 @@ class SqliteRepository(Generic[ItemT]):
     db: Database
     table: SqlTable[ItemT]
 
-    def __iter__(self) -> Iterator[ItemT]:
-        for raw in self.db.execute(self.table.select_all()).fetch_all():
-            yield self.table.load(raw)
-
-    def __len__(self) -> int:
-        raw = self.db.execute(self.table.count_all()).fetch_one()
-
-        try:
-            return int(raw["n_items"])
-        except KeyError:
-            raise UnknownError(raw)
-
-    def delete(self, item_id: str) -> None:
-        self.db.execute(self.table.delete(item_id)).fetch_none()
-
-    def delete_all(self) -> None:
-        self.db.execute(self.table.delete_all()).fetch_none()
-
     def create(self, item: ItemT) -> ItemT:
         try:
             return self.table.load(self.db.execute(self.table.insert(item)).fetch_one())
@@ -69,6 +51,18 @@ class SqliteRepository(Generic[ItemT]):
 
         return self.table.load(raw)
 
+    def __iter__(self) -> Iterator[ItemT]:
+        for raw in self.db.execute(self.table.select_all()).fetch_all():
+            yield self.table.load(raw)
+
+    def __len__(self) -> int:
+        raw = self.db.execute(self.table.count_all()).fetch_one()
+
+        try:
+            return int(raw["n_items"])
+        except KeyError:
+            raise UnknownError(raw)
+
     def update(self, item: ItemT) -> None:
         self.db.execute(self.table.update(item)).fetch_none()
 
@@ -76,18 +70,27 @@ class SqliteRepository(Generic[ItemT]):
         for item in items:
             self.update(item)
 
+    def delete(self, item_id: str) -> None:
+        self.db.execute(self.table.delete(item_id)).fetch_none()
+
+    def delete_all(self) -> None:
+        self.db.execute(self.table.delete_all()).fetch_none()
+
 
 class SqlTable(Protocol[ItemT]):  # pragma: no cover
-    def count_all(self) -> DatabaseCommand:
+    def insert(self, item: ItemT) -> DatabaseCommand:
         raise NotImplementedError("Not implemented")
 
-    def insert(self, item: ItemT) -> DatabaseCommand:
+    def count_all(self) -> DatabaseCommand:
         raise NotImplementedError("Not implemented")
 
     def select(self, item_id: str) -> DatabaseCommand:
         raise NotImplementedError("Not implemented")
 
     def select_all(self) -> DatabaseCommand:
+        raise NotImplementedError("Not implemented")
+
+    def update(self, item: ItemT) -> DatabaseCommand:
         raise NotImplementedError("Not implemented")
 
     def delete(self, item_id: str) -> DatabaseCommand:
@@ -97,9 +100,6 @@ class SqlTable(Protocol[ItemT]):  # pragma: no cover
         raise NotImplementedError("Not implemented")
 
     def load(self, data: dict[str, Any]) -> ItemT:
-        raise NotImplementedError("Not implemented")
-
-    def update(self, item: ItemT) -> DatabaseCommand:
         raise NotImplementedError("Not implemented")
 
 
