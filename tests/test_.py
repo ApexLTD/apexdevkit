25e7d529a208c876b95fad89e48f30e944d4c216
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-08-09 15:09:51 +0400

    Add delete tests

--- tests/test_.py
+++ tests/test_.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 from pytest import fixture, raises
 
 from apexdevkit.error import DoesNotExistError, ExistsError
-from apexdevkit.repository import Database, DatabaseCommand, Repository
+from apexdevkit.repository import Database, DatabaseCommand
 from apexdevkit.repository.connector import SqliteInMemoryConnector
 from apexdevkit.repository.sqlite import SqliteRepository, SqlTable
 
@@ -47,12 +47,18 @@ class FakeTable(SqlTable[_Item]):
             UPDATE ITEM SET id=:id, external_id=:external_id WHERE id=:id;
         """).with_data({"id": item.id, "external_id": item.external_id})
 
+    def delete(self, item_id: str) -> DatabaseCommand:
+        return DatabaseCommand("DELETE FROM ITEM WHERE id=:id").with_data(id=item_id)
+
+    def delete_all(self) -> DatabaseCommand:
+        return DatabaseCommand("DELETE FROM ITEM")
+
     def load(self, data: dict[str, Any]) -> _Item:
         return _Item(data["id"], data["external_id"])
 
 
 @fixture
-def repository() -> Repository[str, _Item]:
+def repository() -> SqliteRepository[_Item]:
     db = Database(SqliteInMemoryConnector())
     db.execute(FakeTable().setup()).fetch_none()
 
@@ -63,23 +69,23 @@ def repository() -> Repository[str, _Item]:
     )
 
 
-def test_should_list_nothing_when_empty(repository: Repository[str, _Item]) -> None:
+def test_should_list_nothing_when_empty(repository: SqliteRepository[_Item]) -> None:
     assert len(repository) == 0
     assert list(repository) == []
 
 
-def test_should_not_read_unknown(repository: Repository[str, _Item]) -> None:
+def test_should_not_read_unknown(repository: SqliteRepository[_Item]) -> None:
     with raises(DoesNotExistError):
         repository.read(str(uuid4()))
 
 
-def test_should_create(repository: Repository[str, _Item]) -> None:
+def test_should_create(repository: SqliteRepository[_Item]) -> None:
     item = _Item(id=str(uuid4()), external_id=str(uuid4()))
 
     assert repository.create(item) == item
 
 
-def test_should_not_duplicate_on_create(repository: Repository[str, _Item]) -> None:
+def test_should_not_duplicate_on_create(repository: SqliteRepository[_Item]) -> None:
     item = _Item(id=str(uuid4()), external_id=str(uuid4()))
     repository.create(item)
 
@@ -87,7 +93,7 @@ def test_should_not_duplicate_on_create(repository: Repository[str, _Item]) -> N
         repository.create(item)
 
 
-def test_should_create_many(repository: Repository[str, _Item]) -> None:
+def test_should_create_many(repository: SqliteRepository[_Item]) -> None:
     items = [
         _Item(id=str(uuid4()), external_id=str(uuid4())),
         _Item(id=str(uuid4()), external_id=str(uuid4())),
@@ -97,7 +103,7 @@ def test_should_create_many(repository: Repository[str, _Item]) -> None:
 
 
 def test_should_not_duplicate_on_create_many(
-    repository: Repository[str, _Item],
+    repository: SqliteRepository[_Item],
 ) -> None:
     items = [
         _Item(id=str(uuid4()), external_id=str(uuid4())),
@@ -109,7 +115,7 @@ def test_should_not_duplicate_on_create_many(
         repository.create_many(items)
 
 
-def test_should_persist(repository: Repository[str, _Item]) -> None:
+def test_should_persist(repository: SqliteRepository[_Item]) -> None:
     item = _Item(id=str(uuid4()), external_id=str(uuid4()))
     repository.create(item)
 
@@ -117,7 +123,7 @@ def test_should_persist(repository: Repository[str, _Item]) -> None:
     assert repository.read(item.id) == item
 
 
-def test_should_persist_many(repository: Repository[str, _Item]) -> None:
+def test_should_persist_many(repository: SqliteRepository[_Item]) -> None:
     items = [
         _Item(id=str(uuid4()), external_id=str(uuid4())),
         _Item(id=str(uuid4()), external_id=str(uuid4())),
@@ -128,7 +134,7 @@ def test_should_persist_many(repository: Repository[str, _Item]) -> None:
     assert list(repository) == items
 
 
-def test_should_persist_update(repository: Repository[str, _Item]) -> None:
+def test_should_persist_update(repository: SqliteRepository[_Item]) -> None:
     old_item = _Item(id=str(uuid4()), external_id=str(uuid4()))
     repository.create(old_item)
 
@@ -138,7 +144,7 @@ def test_should_persist_update(repository: Repository[str, _Item]) -> None:
     assert repository.read(item.id) == item
 
 
-def test_should_persist_update_many(repository: Repository[str, _Item]) -> None:
+def test_should_persist_update_many(repository: SqliteRepository[_Item]) -> None:
     old_items = [
         _Item(id=str(uuid4()), external_id=str(uuid4())),
         _Item(id=str(uuid4()), external_id=str(uuid4())),
@@ -152,3 +158,27 @@ def test_should_persist_update_many(repository: Repository[str, _Item]) -> None:
     repository.update_many(items)
 
     assert list(repository) == items
+
+
+def test_should_persist_delete(repository: SqliteRepository[_Item]) -> None:
+    items = [
+        _Item(id=str(uuid4()), external_id=str(uuid4())),
+        _Item(id=str(uuid4()), external_id=str(uuid4())),
+    ]
+    repository.create_many(items)
+
+    repository.delete(items[1].id)
+
+    assert list(repository) == [items[0]]
+
+
+def test_should_persist_delete_all(repository: SqliteRepository[_Item]) -> None:
+    items = [
+        _Item(id=str(uuid4()), external_id=str(uuid4())),
+        _Item(id=str(uuid4()), external_id=str(uuid4())),
+    ]
+    repository.create_many(items)
+
+    repository.delete_all()
+
+    assert list(repository) == []
