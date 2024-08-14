67ac500f31ab90f0c7e2acec355ee35755c0d16a
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-08-14 17:11:37 +0400

    Test integrity

--- tests/formatter/test_list_formatter.py
+++ tests/formatter/test_list_formatter.py
@@ -33,3 +33,21 @@ def test_should_load() -> None:
     )
 
     assert result == [SampleClass("a", 1), SampleClass("b", 2)]
+
+
+def test_should_retain_dumped_integrity() -> None:
+    formatter = ListFormatter[SampleClass](SampleClassFormatter())
+
+    dumped = formatter.dump([SampleClass("a", 1), SampleClass("b", 2)])
+    formatter.load(dumped)
+
+    assert dumped == [{"name": "a", "age": 1}, {"name": "b", "age": 2}]
+
+
+def test_should_retain_loaded_integrity() -> None:
+    formatter = ListFormatter[SampleClass](SampleClassFormatter())
+
+    loaded = formatter.load([{"name": "a", "age": 1}, {"name": "b", "age": 2}])
+    formatter.dump(loaded)
+
+    assert loaded == [SampleClass("a", 1), SampleClass("b", 2)]
