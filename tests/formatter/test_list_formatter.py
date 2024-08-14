a0b4155a752e1cc7755362af3cd24240149f2643
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-08-14 17:06:39 +0400

    Test dump

---
+++ tests/formatter/test_list_formatter.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Any
+
+from apexdevkit.formatter import ListFormatter
+
+
+@dataclass
+class SampleClass:
+    name: str
+    age: int
+
+
+@dataclass
+class SampleClassFormatter:
+    def load(self, raw: dict[str, Any]) -> SampleClass:
+        return SampleClass(str(raw["name"]), int(raw["age"]))
+
+    def dump(self, item: SampleClass) -> dict[str, Any]:
+        return {"name": item.name, "age": item.age}
+
+
+def test_should_dump() -> None:
+    result = ListFormatter[SampleClass](SampleClassFormatter()).dump(
+        [SampleClass("a", 1), SampleClass("b", 2)]
+    )
+
+    assert result == [{"name": "a", "age": 1}, {"name": "b", "age": 2}]
