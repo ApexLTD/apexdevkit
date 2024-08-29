4d33e9e27bfd8e3b4af20b5261cb93b42d1711f5
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-08-29 14:20:27 +0400

    Extract class

---
+++ apexdevkit/fluent.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Callable, TypeVar, Generic
+
+ItemT = TypeVar("ItemT")
+
+
+class FluentDict(dict[str, ItemT]):
+    def value_of(self, key: str) -> FluentElement:
+        return FluentElement(self[key])
+
+    def and_a(self, **fields: ItemT) -> FluentDict:
+        return self.with_a(**fields)
+
+    def with_a(self, **fields: ItemT) -> FluentDict:
+        return self.merge(FluentDict(fields))
+
+    def merge(self, other: FluentDict) -> FluentDict:
+        return FluentDict({**self, **other})
+
+    def drop(self, *keys: str) -> FluentDict:
+        return self.select(*set(self.keys()).difference(keys))
+
+    def select(self, *keys: str) -> FluentDict:
+        return FluentDict({k: v for k, v in self.items() if k in keys})
+
+
+@dataclass
+class FluentElement(Generic[ItemT]):
+    value: ItemT
+
+    def to(self, a_type: Callable[[ItemT], ConvertedT]) -> ConvertedT:
+        return a_type(self.value)
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+ConvertedT = TypeVar("ConvertedT")
--- apexdevkit/http/json.py
+++ apexdevkit/http/json.py
@@ -1,38 +1,7 @@
-from __future__ import annotations
+from typing import Any
 
-from dataclasses import dataclass
-from typing import Any, Callable, TypeVar
-
-
-class JsonDict(dict[str, Any]):
-    def value_of(self, key: str) -> FluentElement:
-        return FluentElement(self[key])
-
-    def and_a(self, **fields: Any) -> JsonDict:
-        return self.with_a(**fields)
-
-    def with_a(self, **fields: Any) -> JsonDict:
-        return self.merge(JsonDict(fields))
-
-    def merge(self, other: JsonDict) -> JsonDict:
-        return JsonDict({**self, **other})
-
-    def drop(self, *keys: str) -> JsonDict:
-        return self.select(*set(self.keys()).difference(keys))
-
-    def select(self, *keys: str) -> JsonDict:
-        return JsonDict({k: v for k, v in self.items() if k in keys})
+from apexdevkit.fluent import FluentDict
 
 
-@dataclass
-class FluentElement:
-    value: Any
-
-    def to(self, a_type: Callable[[Any], ConvertedT]) -> ConvertedT:
-        return a_type(self.value)
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-
-ConvertedT = TypeVar("ConvertedT")
+class JsonDict(FluentDict[Any]):
+    pass
