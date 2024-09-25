3ed3780faee934495e9b7e4359b74f07f09f8262
RichNachos <gkldi20@freeuni.edu.ge>
2024-09-25 15:38:45 +0400

    Add dataclass repository class

---
+++ apexdevkit/repository/alternative/dataclass_repository.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Any, Generic, Iterator
+
+from apexdevkit.formatter import Formatter
+from apexdevkit.repository.alternative import NewRepositoryBase
+from apexdevkit.repository.alternative.interface import ItemT
+
+_Raw = dict[str, Any]
+
+
+@dataclass
+class DataclassRepository(Generic[ItemT]):
+    base: NewRepositoryBase
+    formatter: Formatter[_Raw, ItemT]
+
+    def create(self, item: ItemT) -> ItemT:
+        return self.formatter.load(self.base.create(self.formatter.dump(item)))
+
+    def create_many(self, items: list[ItemT]) -> list[ItemT]:
+        return [
+            self.formatter.load(item)
+            for item in self.base.create_many(
+                [self.formatter.dump(item) for item in items]
+            )
+        ]
+
+    def read(self, item_id: str) -> ItemT:
+        return self.formatter.load(self.base.read(item_id))
+
+    def update(self, item: ItemT) -> None:
+        self.base.update(self.formatter.dump(item))
+
+    def update_many(self, items: list[ItemT]) -> None:
+        self.base.update_many([self.formatter.dump(item) for item in items])
+
+    def delete(self, item_id: str) -> None:
+        self.base.delete(item_id)
+
+    def __iter__(self) -> Iterator[ItemT]:
+        for raw in self.base:
+            yield self.formatter.load(raw)
+
+    def __len__(self) -> int:
+        return len(self.base)
