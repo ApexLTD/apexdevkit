5aadf3420d9c2d210e270a841689247591c46133
RichNachos <gkldi20@freeuni.edu.ge>
2024-09-25 16:25:04 +0400

    Add tests for dataclass repository

---
+++ tests/repository/alternative/test_dataclass_repository.py
@@ -0,0 +1,140 @@
+from dataclasses import dataclass, replace
+from typing import Any
+
+import pytest
+from _pytest.fixtures import fixture
+from faker.generator import random
+
+from apexdevkit.error import DoesNotExistError, ExistsError
+from apexdevkit.formatter import DataclassFormatter
+from apexdevkit.repository.alternative import NewRepositoryBase, DataclassRepository
+from apexdevkit.testing.fake import Fake
+
+
+@dataclass
+class Person:
+    id: str
+    name: str
+
+
+@fixture
+def repository() -> DataclassRepository[Person]:
+    return DataclassRepository(
+        base=NewRepositoryBase(),
+        formatter=DataclassFormatter[Person](Person),
+    )
+
+
+def fake_person() -> Person:
+    return Person(
+        Fake().uuid(),
+        Fake().last_name(),
+    )
+
+
+def test_should_create(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+    repository.create(fake)
+
+    assert repository.read(fake.id) == fake
+
+
+def test_should_create_many(repository: DataclassRepository[Person]) -> None:
+    fakes = [fake_person() for _ in range(10)]
+    repository.create_many(fakes)
+
+    for fake in fakes:
+        assert repository.read(fake.id) == fake
+
+
+def test_should_read_many(repository: DataclassRepository[Person]) -> None:
+    fakes = [fake_person() for _ in range(10)]
+    repository.create_many(fakes)
+
+    assert fakes == list(repository)
+
+
+def test_should_update(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+    repository.create(fake)
+
+    fake = replace(fake, name="updated")
+
+    repository.update(fake)
+
+    assert fake == repository.read(fake.id)
+
+
+def test_should_update_many(repository: DataclassRepository[Person]) -> None:
+    fakes = [fake_person() for _ in range(10)]
+    repository.create_many(fakes)
+
+    updated_fakes = []
+    for i, fake in enumerate(fakes):
+        updated_fakes.append(replace(fake, name=f"updated_{i}"))
+
+    repository.update_many(updated_fakes)
+
+    assert updated_fakes == list(repository)
+
+
+def test_should_delete(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+    repository.create(fake)
+
+    repository.delete(fake.id)
+
+    with pytest.raises(DoesNotExistError):
+        repository.read(fake.id)
+
+
+def test_correct_length(repository: DataclassRepository[Person]) -> None:
+    random_length = random.randint(1, 10)
+    fakes = [fake_person() for _ in range(random_length)]
+
+    repository.create_many(fakes)
+
+    assert random_length == len(repository)
+
+
+def test_should_not_create_duplicate(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+
+    repository.create(fake)
+
+    with pytest.raises(ExistsError):
+        repository.create(fake)
+
+
+def test_should_not_create_many_duplicates(
+    repository: DataclassRepository[Person],
+) -> None:
+    fakes = [fake_person() for _ in range(10)]
+
+    repository.create_many(fakes)
+
+    with pytest.raises(ExistsError):
+        repository.create_many(fakes)
+
+
+def test_should_not_update_nonexistent(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+
+    with pytest.raises(DoesNotExistError):
+        repository.update(fake)
+
+
+def test_should_not_update_many_nonexistent(
+    repository: DataclassRepository[Person],
+) -> None:
+    fakes = [fake_person() for _ in range(10)]
+
+    with pytest.raises(DoesNotExistError):
+        repository.update_many(fakes)
+
+
+def test_should_not_delete_nonexistent(repository: DataclassRepository[Person]) -> None:
+    fake = fake_person()
+
+    with pytest.raises(DoesNotExistError):
+        repository.delete(fake.id)
