80b55f2a7c9bacfd032eee5d31820210a8083dee
RichNachos <gkldi20@freeuni.edu.ge>
2024-09-25 17:23:56 +0400

    Rename

--- apexdevkit/repository/alternative/__init__.py
+++ apexdevkit/repository/alternative/__init__.py
@@ -1,5 +1,5 @@
 from apexdevkit.repository.alternative.base import NewRepositoryBase
-from apexdevkit.repository.alternative.dataclass_repository import FormatterRepository
+from apexdevkit.repository.alternative.formatter_repository import FormatterRepository
 from apexdevkit.repository.alternative.interface import NewRepository
 
 __all__ = [
