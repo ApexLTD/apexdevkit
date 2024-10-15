0724938c85305a126e77850c177e1acab37e4015
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-10-16 00:03:46 +0400

    Rearrange

--- apexdevkit/repository/in_memory.py
+++ apexdevkit/repository/in_memory.py
@@ -13,6 +13,146 @@ _Raw = dict[str, Any]
 
 
 @dataclass
+class InMemoryRepository(Generic[ItemT]):
+    store: KeyValueStore[ItemT] = field(default_factory=lambda: InMemoryKeyValueStore())
+    keys: list[KeyFunction] = field(default_factory=list)
+    seeds: list[ItemT] = field(default_factory=list)
+
+    def with_formatter(self, value: Formatter[_Raw, ItemT]) -> Self:
+        return self.with_store(InMemoryKeyValueStore(value))
+
+    def with_store(self, value: KeyValueStore[ItemT]) -> Self:
+        self.store = value
+
+        return self
+
+    def and_key(self, function: KeyFunction) -> Self:
+        return self.with_key(function)
+
+    def with_key(self, function: KeyFunction) -> Self:
+        self.keys.append(function)
+
+        return self
+
+    def and_seeded(self, *items: ItemT) -> Self:
+        return self.with_seeded(*items)
+
+    def with_seeded(self, *items: ItemT) -> Self:
+        self.seeds.extend(items)
+
+        return self
+
+    def build(self) -> Repository[ItemT]:
+        return self._seed(self._create())
+
+    def _seed(self, repository: Repository[ItemT]) -> Repository[ItemT]:
+        for seed in self.seeds:
+            repository.create(seed)
+
+        return repository
+
+    def _create(self) -> Repository[ItemT]:
+        match len(self.keys):
+            case 0:
+                return _SingleKeyRepository(self.store)
+            case 1:
+                return _SingleKeyRepository(self.store, pk=self.keys[0])
+            case _:
+                return _ManyKeyRepository(self.store, keys=self.keys)
+
+
+class KeyValueStore(Protocol[ItemT]):
+    def count(self) -> int:
+        pass
+
+    def set(self, key: str, value: ItemT) -> None:
+        pass
+
+    def get(self, key: str) -> ItemT:
+        pass
+
+    def drop(self, key: str) -> None:
+        pass
+
+    def values(self) -> Iterable[ItemT]:
+        pass
+
+
+@dataclass
+class InMemoryKeyValueStore(Generic[ItemT]):
+    formatter: Formatter[_Raw, ItemT] = field(default_factory=NoFormatter)
+    items: dict[str, _Raw] = field(default_factory=dict)
+
+    def count(self) -> int:
+        return len(self.items)
+
+    def set(self, key: str, value: ItemT) -> None:
+        self.items[key] = self.formatter.dump(value)
+
+    def get(self, key: str) -> ItemT:
+        return self.formatter.load(self.items[key])
+
+    def drop(self, key: str) -> None:
+        del self.items[key]
+
+    def values(self) -> Iterable[ItemT]:
+        for raw in self.items.values():
+            yield self.formatter.load(raw)
+
+
+@dataclass
+class _SingleKeyRepository(RepositoryBase[ItemT]):
+    store: KeyValueStore[ItemT]
+
+    pk: KeyFunction = field(default_factory=lambda: AttributeKey("id"))
+
+    def bind(self, **kwargs: Any) -> Self:
+        return self
+
+    def with_seeded(self, *items: ItemT) -> Self:
+        for item in items:
+            self.create(item)
+
+        return self
+
+    def create(self, item: ItemT) -> ItemT:
+        self._ensure_does_not_exist(item)
+        self.store.set(self.pk(item), item)
+
+        return item
+
+    def _ensure_does_not_exist(self, new: ItemT) -> None:
+        try:
+            existing = self.store.get(self.pk(new))
+        except KeyError:
+            return
+
+        ExistsError(existing).with_duplicate(self.pk).fire()
+
+    def update(self, item: ItemT) -> None:
+        self.delete(self.pk(item))
+        self.create(item)
+
+    def delete(self, item_id: str) -> None:
+        try:
+            self.store.drop(item_id)
+        except KeyError:
+            raise DoesNotExistError(item_id)
+
+    def read(self, item_id: str) -> ItemT:
+        try:
+            return self.store.get(item_id)
+        except KeyError:
+            raise DoesNotExistError(item_id)
+
+    def __iter__(self) -> Iterator[ItemT]:
+        return iter(self.store.values())
+
+    def __len__(self) -> int:
+        return self.store.count()
+
+
+@dataclass
 class AttributeKey:
     name: str
 
@@ -80,143 +220,3 @@ class _ManyKeyRepository(RepositoryBase[ItemT]):
 
     def __len__(self) -> int:
         return self.store.count()
-
-
-@dataclass
-class _SingleKeyRepository(RepositoryBase[ItemT]):
-    store: KeyValueStore[ItemT]
-
-    pk: KeyFunction = field(default_factory=lambda: AttributeKey("id"))
-
-    def bind(self, **kwargs: Any) -> Self:
-        return self
-
-    def with_seeded(self, *items: ItemT) -> Self:
-        for item in items:
-            self.create(item)
-
-        return self
-
-    def create(self, item: ItemT) -> ItemT:
-        self._ensure_does_not_exist(item)
-        self.store.set(self.pk(item), item)
-
-        return item
-
-    def _ensure_does_not_exist(self, new: ItemT) -> None:
-        try:
-            existing = self.store.get(self.pk(new))
-        except KeyError:
-            return
-
-        ExistsError(existing).with_duplicate(self.pk).fire()
-
-    def update(self, item: ItemT) -> None:
-        self.delete(self.pk(item))
-        self.create(item)
-
-    def delete(self, item_id: str) -> None:
-        try:
-            self.store.drop(item_id)
-        except KeyError:
-            raise DoesNotExistError(item_id)
-
-    def read(self, item_id: str) -> ItemT:
-        try:
-            return self.store.get(item_id)
-        except KeyError:
-            raise DoesNotExistError(item_id)
-
-    def __iter__(self) -> Iterator[ItemT]:
-        return iter(self.store.values())
-
-    def __len__(self) -> int:
-        return self.store.count()
-
-
-class KeyValueStore(Protocol[ItemT]):
-    def count(self) -> int:
-        pass
-
-    def set(self, key: str, value: ItemT) -> None:
-        pass
-
-    def get(self, key: str) -> ItemT:
-        pass
-
-    def drop(self, key: str) -> None:
-        pass
-
-    def values(self) -> Iterable[ItemT]:
-        pass
-
-
-@dataclass
-class InMemoryKeyValueStore(Generic[ItemT]):
-    formatter: Formatter[_Raw, ItemT] = field(default_factory=NoFormatter)
-    items: dict[str, _Raw] = field(default_factory=dict)
-
-    def count(self) -> int:
-        return len(self.items)
-
-    def set(self, key: str, value: ItemT) -> None:
-        self.items[key] = self.formatter.dump(value)
-
-    def get(self, key: str) -> ItemT:
-        return self.formatter.load(self.items[key])
-
-    def drop(self, key: str) -> None:
-        del self.items[key]
-
-    def values(self) -> Iterable[ItemT]:
-        for raw in self.items.values():
-            yield self.formatter.load(raw)
-
-
-@dataclass
-class InMemoryRepository(Generic[ItemT]):
-    store: KeyValueStore[ItemT] = field(default_factory=InMemoryKeyValueStore)
-    keys: list[KeyFunction] = field(default_factory=list)
-    seeds: list[ItemT] = field(default_factory=list)
-
-    def with_formatter(self, value: Formatter[_Raw, ItemT]) -> Self:
-        return self.with_store(InMemoryKeyValueStore(value))
-
-    def with_store(self, value: KeyValueStore[ItemT]) -> Self:
-        self.store = value
-
-        return self
-
-    def and_key(self, function: KeyFunction) -> Self:
-        return self.with_key(function)
-
-    def with_key(self, function: KeyFunction) -> Self:
-        self.keys.append(function)
-
-        return self
-
-    def and_seeded(self, *items: ItemT) -> Self:
-        return self.with_seeded(*items)
-
-    def with_seeded(self, *items: ItemT) -> Self:
-        self.seeds.extend(items)
-
-        return self
-
-    def build(self) -> Repository[ItemT]:
-        return self._seed(self._create())
-
-    def _seed(self, repository: Repository[ItemT]) -> Repository[ItemT]:
-        for seed in self.seeds:
-            repository.create(seed)
-
-        return repository
-
-    def _create(self) -> Repository[ItemT]:
-        match len(self.keys):
-            case 0:
-                return _SingleKeyRepository(self.store)
-            case 1:
-                return _SingleKeyRepository(self.store, pk=self.keys[0])
-            case _:
-                return _ManyKeyRepository(self.store, keys=self.keys)
