18dd0bc0884b2901dce73bcbadd8c0eccc075265
itodadze <itoda20@freeuni.edu.ge>
2024-11-04 13:02:28 +0400

    Remove class

--- apexdevkit/value.py
+++ apexdevkit/value.py
@@ -36,37 +36,6 @@ class Quantity:
 
 
 @dataclass(frozen=True)
-class Value:
-    value: int = 0
-    exponent: int = 1
-
-    unit: str = "unknown"
-
-    def as_decimal(self) -> Decimal:
-        return Decimal(self.value) / Decimal(self.exponent)
-
-    def as_quantity(self) -> Quantity:
-        return Quantity(self.value, self.exponent)
-
-    @classmethod
-    def from_string(cls, decimal_str: str) -> Value:
-        quantity = Quantity.from_string(decimal_str)
-        return cls(quantity.value, quantity.exponent)
-
-    def add(self, other: Value) -> Value:
-        return Value(
-            self.as_quantity().add(other.as_quantity()).value, self.exponent, self.unit
-        )
-
-    def subtract(self, other: Value) -> Value:
-        return Value(
-            self.as_quantity().subtract(other.as_quantity()).value,
-            self.exponent,
-            self.unit,
-        )
-
-
-@dataclass(frozen=True)
 class FakeQuantity(FakeResource[Quantity]):
     item_type: Type[Quantity] = field(default=Quantity)
 
@@ -78,30 +47,9 @@ class FakeQuantity(FakeResource[Quantity]):
         }
 
 
-@dataclass(frozen=True)
-class FakeValue(FakeResource[Value]):
-    item_type: Type[Value] = field(default=Value)
-
-    @cached_property
-    def _raw(self) -> dict[str, Any]:
-        return {
-            "value": self.fake.number(),
-            "exponent": random.choice([10, 100, 1000]),
-            "unit": random.choice(["GEL", "EUR", "KG"]),
-        }
-
-
 class QuantityFormatter:
     def load(self, raw: dict[str, Any]) -> Quantity:
         return DataclassFormatter(Quantity).load(raw)
 
     def dump(self, quantity: Quantity) -> dict[str, Any]:
         return DataclassFormatter(Quantity).dump(quantity)
-
-
-class ValueFormatter:
-    def load(self, raw: dict[str, Any]) -> Value:
-        return DataclassFormatter(Value).load(raw)
-
-    def dump(self, value: Value) -> dict[str, Any]:
-        return DataclassFormatter(Value).dump(value)
