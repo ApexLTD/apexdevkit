d4a2b0f1811e5002506775ae58b36613f4706abf
Irakli Todadze <72357245+itodadze@users.noreply.github.com>
2024-11-18 14:28:16 +0400

    MsSql table generator (#209)

--- apexdevkit/repository/mssql.py
+++ apexdevkit/repository/mssql.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Generic, Iterator, TypeVar
+from typing import Any, Generic, Iterable, Iterator, TypeVar
 
 from pymssql.exceptions import DatabaseError
 
 from apexdevkit.error import DoesNotExistError, ExistsError
+from apexdevkit.formatter import Formatter
 from apexdevkit.repository import Database, DatabaseCommand, RepositoryBase
 
 ItemT = TypeVar("ItemT")
@@ -87,6 +88,38 @@ class SqlTable(Generic[ItemT]):  # pragma: no cover
 
 
 @dataclass
+class SqlTableDecorator(Generic[ItemT]):
+    table: DefaultSqlTable[ItemT]
+
+    def count_all(self) -> DatabaseCommand:
+        return self.table.count_all()
+
+    def insert(self, item: ItemT) -> DatabaseCommand:
+        return self.table.insert(item)
+
+    def select(self, item_id: str) -> DatabaseCommand:
+        return self.table.select(item_id)
+
+    def select_all(self) -> DatabaseCommand:
+        return self.table.select_all()
+
+    def delete(self, item_id: str) -> DatabaseCommand:
+        return self.table.delete(item_id)
+
+    def delete_all(self) -> DatabaseCommand:
+        return self.table.delete_all()
+
+    def update(self, item: ItemT) -> DatabaseCommand:
+        return self.table.update(item)
+
+    def load(self, data: dict[str, Any]) -> ItemT:
+        return self.table.load(data)
+
+    def exists(self, duplicate: ItemT) -> ExistsError:
+        return self.table.exists(duplicate)
+
+
+@dataclass
 class MssqlException:
     code: int
     message: str
@@ -102,3 +135,333 @@ class MssqlException:
 @dataclass
 class UnknownError(Exception):
     raw: Any
+
+
+@dataclass(frozen=True)
+class MsSqlTableBuilder(Generic[ItemT]):
+    username: str | None = None
+    schema: str | None = None
+    table: str | None = None
+    formatter: Formatter[dict[str, Any], ItemT] | None = None
+    fields: list[MsSqlField] | None = None
+    ordering: list[str] | None = None
+    parent_field: str | None = None
+    parent_value: Any | None = None
+
+    def with_username(self, value: str) -> MsSqlTableBuilder[ItemT]:
+        return MsSqlTableBuilder[ItemT](
+            value,
+            self.schema,
+            self.table,
+            self.formatter,
+            self.fields,
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_schema(self, value: str) -> MsSqlTableBuilder[ItemT]:
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            value,
+            self.table,
+            self.formatter,
+            self.fields,
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_table(self, value: str) -> MsSqlTableBuilder[ItemT]:
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            value,
+            self.formatter,
+            self.fields,
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_formatter(
+        self, value: Formatter[dict[str, Any], ItemT]
+    ) -> MsSqlTableBuilder[ItemT]:
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            self.table,
+            value,
+            self.fields,
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_fields(self, fields: Iterable[str]) -> MsSqlTableBuilder[ItemT]:
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            self.table,
+            self.formatter,
+            [MsSqlField(field, field == "id") for field in list(fields)],
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_order_fields(self, ordering: Iterable[str]) -> MsSqlTableBuilder[ItemT]:
+        ordered = list(ordering)
+        assert self.fields is not None, "Set fields first."
+
+        for key in ordered:
+            if key not in [field.name for field in self.fields]:
+                raise ValueError("Missing fields in the table.")
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            self.table,
+            self.formatter,
+            self.fields,
+            ordered,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_id(self, identifier: str) -> MsSqlTableBuilder[ItemT]:
+        assert self.fields is not None, "Set fields first."
+        if identifier not in [field.name for field in self.fields]:
+            raise ValueError("Missing fields in the table.")
+
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            self.table,
+            self.formatter,
+            [MsSqlField(field.name, field.name == identifier) for field in self.fields],
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+    def with_parent(
+        self, parent_field: str, parent_value: Any
+    ) -> MsSqlTableBuilder[ItemT]:
+        assert self.fields is not None, "Set fields first."
+        if parent_field not in [field.name for field in self.fields]:
+            raise ValueError("Missing fields in the table.")
+
+        return MsSqlTableBuilder[ItemT](
+            self.username,
+            self.schema,
+            self.table,
+            self.formatter,
+            self.fields,
+            self.ordering,
+            parent_field,
+            parent_value,
+        )
+
+    def build(self) -> SqlTable[ItemT]:
+        if not self.schema or not self.table or not self.formatter or not self.fields:
+            raise ValueError("Cannot build sql table.")
+
+        return DefaultSqlTable(
+            self.username,
+            self.schema,
+            self.table,
+            self.formatter,
+            self.fields,
+            self.ordering,
+            self.parent_field,
+            self.parent_value,
+        )
+
+
+@dataclass(frozen=True)
+class DefaultSqlTable(SqlTable[ItemT]):
+    username: str | None
+    schema: str
+    table: str
+    formatter: Formatter[dict[str, Any], ItemT]
+    fields: list[MsSqlField]
+    ordering: list[str] | None
+    parent_key: str | None
+    parent_value: Any | None
+
+    def count_all(self) -> DatabaseCommand:
+        where_statement = ""
+        if self.parent_key is not None:
+            where_statement += (
+                "WHERE [" + self.parent_key + "] = " + self._parent_value_sql
+            )
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            SELECT count(*) AS n_items
+            FROM [{self.schema}].[{self.table}]
+            {where_statement}
+            REVERT
+        """)
+
+    def insert(self, item: ItemT) -> DatabaseCommand:
+        dumped = self.formatter.dump(item)
+        if self.parent_key is not None:
+            dumped[self.parent_key] = self.parent_value
+
+        columns = ", ".join(["[" + field.name + "]" for field in self.fields])
+        placeholders = ", ".join([f"%({key.name})s" for key in self.fields])
+        output = ", ".join(["INSERTED." + field.name for field in self.fields])
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            INSERT INTO [{self.schema}].[{self.table}] (
+                {columns}
+            ) OUTPUT
+                {output}
+            VALUES (
+                {placeholders}
+            )
+            REVERT
+        """).with_data(dumped)
+
+    def select(self, item_id: str) -> DatabaseCommand:
+        raw: dict[str, Any] = {self._id: item_id}
+        where_statement = f"WHERE [{self._id}] = %({self._id})s"
+        if self.parent_key is not None:
+            raw[self.parent_key] = self.parent_value
+            where_statement += (
+                ", [" + self.parent_key + "] = %(" + self.parent_key + ")s"
+            )
+
+        columns = ", ".join(["[" + field.name + "]" for field in self.fields])
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            SELECT
+                {columns} 
+            FROM [{self.schema}].[{self.table}]
+            {where_statement}
+            REVERT
+        """).with_data(raw)
+
+    def select_all(self) -> DatabaseCommand:
+        columns = ", ".join(["[" + field.name + "]" for field in self.fields])
+        where_statement = ""
+        if self.parent_key is not None:
+            where_statement += (
+                "WHERE [" + self.parent_key + "] = " + self._parent_value_sql
+            )
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            SELECT
+                {columns}
+            FROM [{self.schema}].[{self.table}]
+            {where_statement}
+            {self._order}
+            REVERT
+        """)
+
+    def update(self, item: ItemT) -> DatabaseCommand:
+        dumped = self.formatter.dump(item)
+        if self.parent_key is not None:
+            dumped[self.parent_key] = self.parent_value
+
+        where_statement = f"WHERE [{self._id}] = %({self._id})s"
+        if self.parent_key is not None:
+            where_statement += (
+                ", [" + self.parent_key + "] = %(" + self.parent_key + ")s"
+            )
+
+        updates = ", ".join(
+            [
+                f"{field.name} = %({field.name})s"
+                for field in self.fields
+                if not field.is_id and field.name != self.parent_key
+            ]
+        )
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            UPDATE [{self.schema}].[{self.table}]
+            SET
+                {updates}
+            {where_statement}
+            REVERT
+        """).with_data(dumped)
+
+    def delete(self, item_id: str) -> DatabaseCommand:
+        raw: dict[str, Any] = {self._id: item_id}
+        where_statement = f"WHERE [{self._id}] = %({self._id})s"
+        if self.parent_key is not None:
+            raw[self.parent_key] = self.parent_value
+            where_statement += (
+                ", [" + self.parent_key + "] = %(" + self.parent_key + ")s"
+            )
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            DELETE
+            FROM [{self.schema}].[{self.table}]
+            {where_statement}
+            REVERT
+        """).with_data(raw)
+
+    def delete_all(self) -> DatabaseCommand:
+        where_statement = ""
+        if self.parent_key is not None:
+            where_statement += (
+                "WHERE [" + self.parent_key + "] = " + self._parent_value_sql
+            )
+
+        return DatabaseCommand(f"""
+            {self._user_check}
+            DELETE
+            FROM [{self.schema}].[{self.table}]
+            {where_statement}
+            REVERT
+        """)
+
+    def load(self, data: dict[str, Any]) -> ItemT:
+        return self.formatter.load(data)
+
+    def exists(self, duplicate: ItemT) -> ExistsError:
+        raw = self.formatter.dump(duplicate)
+        return ExistsError(duplicate).with_duplicate(
+            lambda i: f"{self._id}<{raw[self._id]}>"
+        )
+
+    @property
+    def _id(self) -> str:
+        result = next((field for field in self.fields if field.is_id), None)
+        if result is None:
+            raise ValueError("Id field is required.")
+        return result.name
+
+    @property
+    def _user_check(self) -> str:
+        if self.username is not None:
+            return f"EXECUTE AS USER = '{self.username}'"
+        else:
+            return ""
+
+    @property
+    def _order(self) -> str:
+        if self.ordering is not None and len(self.ordering) > 0:
+            return "ORDER BY " + ", ".join(self.ordering)
+        else:
+            return ""
+
+    @property
+    def _parent_value_sql(self) -> str:
+        if isinstance(self.parent_value, str):
+            return "'" + self.parent_value + "'"
+        else:
+            return str(self.parent_value)
+
+
+@dataclass(frozen=True)
+class MsSqlField:
+    name: str
+    is_id: bool
---
+++ tests/repository/test_mssql_table.py
@@ -0,0 +1,287 @@
+from dataclasses import dataclass, field
+from typing import Any
+from uuid import uuid4
+
+from pytest import fixture
+
+from apexdevkit.repository import DatabaseCommand
+from apexdevkit.repository.mssql import MsSqlTableBuilder, SqlTable
+
+
+@dataclass
+class Apple:
+    color: str
+    parent: str | None
+
+    id: str = field(default_factory=lambda: str(uuid4()))
+
+
+@dataclass
+class AppleFormatter:
+    def dump(self, data: Apple) -> dict[str, Any]:
+        return {"apid": data.id, "clr": data.color, "pid": data.parent}
+
+    def load(self, raw: dict[str, Any]) -> Apple:
+        return Apple(raw["clr"], raw["pid"], raw["apid"])
+
+
+@fixture
+def table() -> SqlTable[Apple]:
+    return (
+        MsSqlTableBuilder()
+        .with_username("test")
+        .with_schema("test")
+        .with_table("apples")
+        .with_fields(["apid", "clr", "pid"])
+        .with_id("apid")
+        .with_order_fields(["apid"])
+        .with_formatter(AppleFormatter())  # type: ignore
+        .build()
+    )
+
+
+@fixture()
+def table_with_parent() -> SqlTable[Apple]:
+    return (
+        MsSqlTableBuilder()
+        .with_username("test")
+        .with_schema("test")
+        .with_table("apples")
+        .with_fields(["apid", "clr", "pid"])
+        .with_id("apid")
+        .with_order_fields(["apid"])
+        .with_parent("pid", "test")
+        .with_formatter(AppleFormatter())  # type: ignore
+        .build()
+    )
+
+
+@fixture
+def apple() -> Apple:
+    return Apple("red", "1", "1")
+
+
+def test_should_count(table: SqlTable[Apple]) -> None:
+    command = table.count_all()
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            SELECT count(*) AS n_items
+            FROM [test].[apples]
+            
+            REVERT
+        """
+    )
+
+
+def test_should_insert(table: SqlTable[Apple], apple: Apple) -> None:
+    command = table.insert(apple)
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            INSERT INTO [test].[apples] (
+                [apid], [clr], [pid]
+            ) OUTPUT
+                INSERTED.apid, INSERTED.clr, INSERTED.pid
+            VALUES (
+                %(apid)s, %(clr)s, %(pid)s
+            )
+            REVERT
+        """
+    ).with_data(AppleFormatter().dump(apple))
+
+
+def test_should_select(table: SqlTable[Apple], apple: Apple) -> None:
+    command = table.select(apple.id)
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            SELECT
+                [apid], [clr], [pid] 
+            FROM [test].[apples]
+            WHERE [apid] = %(apid)s
+            REVERT
+        """
+    ).with_data(apid=apple.id)
+
+
+def test_should_select_all(table: SqlTable[Apple]) -> None:
+    command = table.select_all()
+    assert (
+        command
+        == DatabaseCommand(
+            """
+            EXECUTE AS USER = 'test'
+            SELECT
+                [apid], [clr], [pid]
+            FROM [test].[apples]
+            
+            ORDER BY apid
+            REVERT
+        """
+        ).with_data()
+    )
+
+
+def test_should_update(table: SqlTable[Apple], apple: Apple) -> None:
+    command = table.update(apple)
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            UPDATE [test].[apples]
+            SET
+                clr = %(clr)s, pid = %(pid)s
+            WHERE [apid] = %(apid)s
+            REVERT
+        """
+    ).with_data(AppleFormatter().dump(apple))
+
+
+def test_should_delete(table: SqlTable[Apple], apple: Apple) -> None:
+    command = table.delete(apple.id)
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            DELETE
+            FROM [test].[apples]
+            WHERE [apid] = %(apid)s
+            REVERT
+        """
+    ).with_data(apid=apple.id)
+
+
+def test_should_delete_all(table: SqlTable[Apple]) -> None:
+    command = table.delete_all()
+    assert (
+        command
+        == DatabaseCommand(
+            """
+            EXECUTE AS USER = 'test'
+            DELETE
+            FROM [test].[apples]
+            
+            REVERT
+        """
+        ).with_data()
+    )
+
+
+def test_should_count_with_parent(table_with_parent: SqlTable[Apple]) -> None:
+    command = table_with_parent.count_all()
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            SELECT count(*) AS n_items
+            FROM [test].[apples]
+            WHERE [pid] = 'test'
+            REVERT
+        """
+    )
+
+
+def test_should_insert_with_parent(
+    table_with_parent: SqlTable[Apple], apple: Apple
+) -> None:
+    command = table_with_parent.insert(apple)
+    dumped = AppleFormatter().dump(apple)
+    dumped["pid"] = "test"
+
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            INSERT INTO [test].[apples] (
+                [apid], [clr], [pid]
+            ) OUTPUT
+                INSERTED.apid, INSERTED.clr, INSERTED.pid
+            VALUES (
+                %(apid)s, %(clr)s, %(pid)s
+            )
+            REVERT
+        """
+    ).with_data(dumped)
+
+
+def test_should_select_with_parent(
+    table_with_parent: SqlTable[Apple], apple: Apple
+) -> None:
+    command = table_with_parent.select(apple.id)
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            SELECT
+                [apid], [clr], [pid] 
+            FROM [test].[apples]
+            WHERE [apid] = %(apid)s, [pid] = %(pid)s
+            REVERT
+        """
+    ).with_data(apid=apple.id, pid="test")
+
+
+def test_should_select_all_with_parent(table_with_parent: SqlTable[Apple]) -> None:
+    command = table_with_parent.select_all()
+    assert (
+        command
+        == DatabaseCommand(
+            """
+            EXECUTE AS USER = 'test'
+            SELECT
+                [apid], [clr], [pid]
+            FROM [test].[apples]
+            WHERE [pid] = 'test'
+            ORDER BY apid
+            REVERT
+        """
+        ).with_data()
+    )
+
+
+def test_should_update_with_parent(
+    table_with_parent: SqlTable[Apple], apple: Apple
+) -> None:
+    command = table_with_parent.update(apple)
+    dumped = AppleFormatter().dump(apple)
+    dumped["pid"] = "test"
+
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            UPDATE [test].[apples]
+            SET
+                clr = %(clr)s
+            WHERE [apid] = %(apid)s, [pid] = %(pid)s
+            REVERT
+        """
+    ).with_data(dumped)
+
+
+def test_should_delete_with_parent(
+    table_with_parent: SqlTable[Apple], apple: Apple
+) -> None:
+    command = table_with_parent.delete(apple.id)
+
+    assert command == DatabaseCommand(
+        """
+            EXECUTE AS USER = 'test'
+            DELETE
+            FROM [test].[apples]
+            WHERE [apid] = %(apid)s, [pid] = %(pid)s
+            REVERT
+        """
+    ).with_data(apid=apple.id, pid="test")
+
+
+def test_should_delete_all_with_parent(table_with_parent: SqlTable[Apple]) -> None:
+    command = table_with_parent.delete_all()
+
+    assert (
+        command
+        == DatabaseCommand(
+            """
+            EXECUTE AS USER = 'test'
+            DELETE
+            FROM [test].[apples]
+            WHERE [pid] = 'test'
+            REVERT
+        """
+        ).with_data()
+    )
