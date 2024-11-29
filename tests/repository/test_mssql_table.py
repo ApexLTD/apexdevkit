ecdf2afdd6a1641bc80e4e2e375b3b6b94dc6198
Irakli Todadze <72357245+itodadze@users.noreply.github.com>
2024-11-29 15:28:36 +0400

    Fix select on insert (#228)

--- tests/repository/test_mssql_table.py
+++ tests/repository/test_mssql_table.py
@@ -107,7 +107,7 @@ def test_should_insert(table: SqlTable[Apple], apple: Apple) -> None:
         + """ [manager] AS manager
             
                 FROM [test].[apples]
-                WHERE [apid] = SCOPE_IDENTITY() AND [manager] IS NOT NULL
+                WHERE [apid] = %(apid)s AND [manager] IS NOT NULL
             
             REVERT
         """
@@ -217,7 +217,7 @@ def test_should_insert_with_parent(
                 [apid] AS apid, [clr] AS clr, [pid] AS pid
             
                 FROM [test].[apples]
-                WHERE [pid] = %(pid)s AND [apid] = SCOPE_IDENTITY()
+                WHERE [pid] = %(pid)s AND [apid] = %(apid)s
             
             REVERT
         """
