ce2908f90b8b134080f9fc286a9ce6b228b1fc29
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2024-12-06 13:17:39 +0400

    Refactor test

--- tests/query/test_selection.py
+++ tests/query/test_selection.py
@@ -2,12 +2,11 @@ from apexdevkit.query.generator import MsSqlSelectionGenerator
 
 
 def test_should_generate_selection() -> None:
-    assert (
-        MsSqlSelectionGenerator(
-            {
-                "test_field": "field",
-                "test_field_2": "field_2",
-            }
-        ).generate()
-        == "SELECT [field] AS [test_field], [field_2] AS [test_field_2]"
-    )
+    fields = {
+        "test_field": "field",
+        "test_field_2": "field_2",
+    }
+
+    result = MsSqlSelectionGenerator(fields).generate()
+
+    assert result == "SELECT [field] AS [test_field], [field_2] AS [test_field_2]"
