8e847f20bba0ad6d9833843f2a509dcd65784da5
RichNachos <91462430+RichNachos@users.noreply.github.com>
2024-12-09 17:01:44 +0400

    Add Apex ID generator class (#232)

---
+++ apexdevkit/id.py
@@ -0,0 +1,54 @@
+import os
+from threading import Lock
+from time import time
+
+
+class ApexID:
+    last_timestamp: int = -1
+    sequence: int = 0
+    lock: Lock = Lock()
+
+    _metadata_bitmask: int = 0x0FFF
+    _sequence_bitmask: int = 0x03FF
+    _timestamp_bitmask: int = 0x03FFFFFFFFFF
+
+    @classmethod
+    def id(cls) -> int:
+        """
+        Generates a unique identifier with the following structure
+        from most significant to least significant:
+        1 bit reserved
+        41 bits for unix timestamp (millisecond precision)
+        12 bits for metadata
+        10 bits for sequence
+        """
+        with cls.lock:
+            current_timestamp = cls._get_timestamp()
+            if cls.last_timestamp == current_timestamp:
+                cls.sequence += 1
+            else:
+                cls.sequence = 0
+
+            if cls.sequence > cls._sequence_bitmask:
+                raise DuplicateIDException("Duplicate ID Generated")
+
+            cls.last_timestamp = current_timestamp
+
+        return (
+            (0 << 63)
+            | ((cls.last_timestamp & cls._timestamp_bitmask) << 22)
+            | ((cls._metadata() & cls._metadata_bitmask) << 10)
+            | (cls.sequence & cls._sequence_bitmask)
+        )
+
+    @classmethod
+    def _get_timestamp(cls) -> int:
+        return int(time() * 1000)
+
+    @classmethod
+    def _metadata(cls) -> int:
+        return int(os.getenv("APEX_ID_METADATA", "0"))
+
+
+class DuplicateIDException(Exception):
+    pass
---
+++ tests/test_id.py
@@ -0,0 +1,39 @@
+import os
+
+from apexdevkit.id import ApexID
+
+
+def test_should_generate_unique() -> None:
+    ids_set = set()
+    for i in range(ApexID._sequence_bitmask):
+        generated_id = ApexID.id()
+
+        assert generated_id not in ids_set
+
+        ids_set.add(generated_id)
+
+
+def test_should_encode_sequence_id() -> None:
+    assert ApexID.id() & ApexID._sequence_bitmask == ApexID.sequence
+
+
+def test_should_encode_metadata() -> None:
+    os.environ["APEX_ID_METADATA"] = "1000"
+
+    assert ApexID.id() >> 10 & ApexID._metadata_bitmask == 1000
+
+
+def test_should_encode_timestamp() -> None:
+    assert ApexID.id() >> 22 & ApexID._timestamp_bitmask == ApexID.last_timestamp
+
+
+def test_should_sort_in_generated_order() -> None:
+    ids = []
+
+    for i in range(10_000):
+        ids.append(ApexID.id())
+
+    sorted_ids = ids.copy()
+    sorted_ids.sort()
+
+    assert ids == sorted_ids
