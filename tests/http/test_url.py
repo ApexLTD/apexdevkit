dcb3a20009bc9185428a753d5893c40a5df69643
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2025-01-20 12:28:07 +0400

    Rename

--- tests/http/test_url.py
+++ tests/http/test_url.py
@@ -1,25 +1,25 @@
 from apexdevkit.http.url import HttpUrl
 
 
-def test_should_not_add_trailing_stalsh_for_empty_endpoint() -> None:
+def test_should_not_add_trailing_slash_for_empty_endpoint() -> None:
     url = HttpUrl("https://www.example.com")
 
     assert url + "" == "https://www.example.com"
 
 
-def test_httpurl_add_endpoint_to_value() -> None:
+def test_should_add_endpoint_to_base() -> None:
     url = HttpUrl("https://www.example.com")
 
     assert url + "test" == "https://www.example.com/test"
 
 
-def test_httpurl_add_endpoint_to_value_with_trailing_slash() -> None:
+def test_should_add_endpoint_to_base_with_trailing_slash() -> None:
     url = HttpUrl("https://www.example.com/")
 
     assert url + "test" == "https://www.example.com/test"
 
 
-def test_httpurl_add_endpoint_with_leading_slash() -> None:
+def test_should_add_endpoint_with_leading_slash() -> None:
     url = HttpUrl("https://www.example.com")
 
     assert url + "/test" == "https://www.example.com/test"
