7444aeab4973dc5d73cb3e44aa6a8d679a32849f
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2025-02-12 13:42:08 +0400

    Rearrange

--- tests/fastapi/test_dependable.py
+++ tests/fastapi/test_dependable.py
@@ -14,34 +14,6 @@ from apexdevkit.testing import RestCollection
 from tests.fastapi.sample_api import AppleFields, PriceFields
 
 
-def resource(dependency: Dependency) -> RestCollection:
-    return RestCollection(
-        name=RestfulName("apple"),
-        http=Httpx(
-            TestClient(
-                FastApiBuilder()
-                .with_route(
-                    apples=RestfulRouter()
-                    .with_name(RestfulName("apple"))
-                    .with_fields(AppleFields())
-                    .with_dependency(dependency)
-                    .with_sub_resource(
-                        prices=RestfulRouter()
-                        .with_name(RestfulName("price"))
-                        .with_fields(PriceFields())
-                        .with_dependency(dependency)
-                        .default()
-                        .build()
-                    )
-                    .default()
-                    .build()
-                )
-                .build()
-            )
-        ),
-    )
-
-
 @pytest.fixture
 def user() -> Any:
     return "A"
@@ -68,6 +40,34 @@ def parent() -> RestfulName:
 @pytest.fixture
 def child() -> RestfulName:
     return RestfulName("price")
+
+
+def resource(dependency: Dependency) -> RestCollection:
+    return RestCollection(
+        name=RestfulName("apple"),
+        http=Httpx(
+            TestClient(
+                FastApiBuilder()
+                .with_route(
+                    apples=RestfulRouter()
+                    .with_name(RestfulName("apple"))
+                    .with_fields(AppleFields())
+                    .with_dependency(dependency)
+                    .with_sub_resource(
+                        prices=RestfulRouter()
+                        .with_name(RestfulName("price"))
+                        .with_fields(PriceFields())
+                        .with_dependency(dependency)
+                        .default()
+                        .build()
+                    )
+                    .default()
+                    .build()
+                )
+                .build()
+            )
+        ),
+    )
 
 
 def test_should_build_dependable_with_user(
