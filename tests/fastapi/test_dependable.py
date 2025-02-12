8b0510a698153ba5f860bedc96f0108b2721138a
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2025-02-12 13:46:10 +0400

    Simplify mock

--- tests/fastapi/test_dependable.py
+++ tests/fastapi/test_dependable.py
@@ -62,15 +62,13 @@ def test_should_build_dependable_with_user(faker: Faker) -> None:
     builder = MagicMock(spec=RestfulServiceBuilder)
 
     (
-        resource(
-            DependableBuilder.from_callable(lambda: builder).with_user(lambda: user)
-        )
+        resource(DependableBuilder.from_callable(builder).with_user(lambda: user))
         .read_all()
         .ensure()
     )
 
-    builder.with_user.assert_called_once_with(user)
-    builder.with_user().build.assert_called_once()
+    builder().with_user.assert_called_once_with(user)
+    builder().with_user().build.assert_called_once()
 
 
 def test_should_build_dependable_with_parent(
