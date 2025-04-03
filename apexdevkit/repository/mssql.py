f845403773906107541ea69b98ebf26e6bd86d0c
Irakli Todadze <72357245+itodadze@users.noreply.github.com>
2025-04-03 15:58:02 +0400

    Add temporary type conversion fix to mssql read one (#265)

--- apexdevkit/repository/mssql.py
+++ apexdevkit/repository/mssql.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any, Generic, Iterable, Iterator, Mapping, TypeVar
 
-from pymssql.exceptions import DatabaseError
+from pymssql.exceptions import DatabaseError, OperationalError
 
 from apexdevkit.error import DoesNotExistError, ExistsError
 from apexdevkit.formatter import Formatter
@@ -48,7 +48,13 @@ class MsSqlRepository(RepositoryBase[ItemT]):
             raise UnknownError(e.message)
 
     def read(self, item_id: str) -> ItemT:
-        raw = self.db.execute(self.table.select(item_id)).fetch_one()
+        try:
+            raw = self.db.execute(self.table.select(item_id)).fetch_one()
+        except OperationalError as e:
+            if "Conversion failed" in str(e):
+                raise DoesNotExistError(item_id)
+            else:
+                raise e
 
         if not raw:
             raise DoesNotExistError(item_id)
