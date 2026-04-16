02cc896c12c63f314bcc3b90405336942acb3c09
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-04-16 13:46:29 +0400

    Rename

--- tests/approvals/repository/extension.py
+++
@@ -1,9 +0,0 @@
-from approvaltests import verify
-from approvaltests.core import Options
-
-
-def verify_sql(code: str) -> None:
-    verify(
-        "\n".join([line if line.strip() else "" for line in code.split("\n")]),
-        options=Options({"extension_with_dot": ".sql"}),
-    )
