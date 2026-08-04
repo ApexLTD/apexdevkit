9c467388612c2aa2e59e722a9afdd791433edf94
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-04 15:32:20 +0400

    Move

--- apexdevkit/repository/sql/sqlite/table.py
+++ apexdevkit/repository/sql/sqlite/table.py
@@ -1,68 +1,13 @@
 from __future__ import annotations
 
-from collections.abc import Iterable, Iterator, Mapping
-from dataclasses import dataclass, field
-from sqlite3 import IntegrityError
+from collections.abc import Iterable, Mapping
+from dataclasses import dataclass
 from typing import Any, Generic
 
-from apexdevkit.error import DoesNotExistError, ExistsError
+from apexdevkit.error import ExistsError
 from apexdevkit.formatter import Formatter
-from apexdevkit.repository.core import (
-    ContainsMixin,
-    Database,
-    DatabaseCommand,
-    ItemT,
-    Repository,
-)
-
-from .connector import SqliteFileConnector
-from .field import NotNone, SqlFieldManager, _SqlField
-
-
-@dataclass(frozen=True, kw_only=True)
-class SqliteRepository(ContainsMixin, Repository[ItemT]):
-    table: SqlTable[ItemT]
-
-    db: Database = field(default_factory=lambda: Database(SqliteFileConnector()))
-
-    def __iter__(self) -> Iterator[ItemT]:
-        for raw in self.db.execute(self.table.select_all()).fetch_all():
-            yield self.table.load(raw)
-
-    def __len__(self) -> int:
-        raw = self.db.execute(self.table.count_all()).fetch_one()
-
-        try:
-            return int(raw["n_items"])
-        except KeyError as e:
-            raise UnknownError(raw) from e
-
-    def create(self, item: ItemT) -> ItemT:
-        try:
-            return self.table.load(self.db.execute(self.table.insert(item)).fetch_one())
-        except IntegrityError:  # pragma: no cover
-            item = self.table.load(
-                self.db.execute(self.table.select_duplicate(item)).fetch_one()
-            )
-            self.table.duplicate(item).fire()
-            return item
-
-    def read(self, item_id: str) -> ItemT:
-        raw = self.db.execute(self.table.select(str(item_id))).fetch_one()
-
-        if not raw:
-            raise DoesNotExistError(item_id)
-
-        return self.table.load(raw)
-
-    def update(self, item: ItemT) -> None:
-        self.db.execute(self.table.update(item)).fetch_none()
-
-    def delete(self, item_id: str) -> None:
-        self.db.execute(self.table.delete(str(item_id))).fetch_none()
-
-    def delete_all(self) -> None:
-        self.db.execute(self.table.delete_all()).fetch_none()
+from apexdevkit.repository.core import DatabaseCommand, ItemT
+from apexdevkit.repository.sql.field import NotNone, SqlFieldManager, _SqlField
 
 
 class SqlTable(Generic[ItemT]):  # pragma: no cover
@@ -95,11 +40,6 @@ class SqlTable(Generic[ItemT]):  # pragma: no cover
 
     def duplicate(self, item: ItemT) -> ExistsError:
         return ExistsError(item).with_duplicate(lambda _: "Unknown")
-
-
-@dataclass
-class UnknownError(Exception):
-    raw: Mapping[str, Any]
 
 
 @dataclass(frozen=True)
