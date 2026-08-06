e4c2fe9dbd88786a0d6885a8a3d5f926eb69a2ab
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-06 17:48:10 +0400

    Use environment from pypebbles

--- apexdevkit/environment.py
+++ apexdevkit/environment.py
@@ -1,10 +1,10 @@
-import os
-from dataclasses import field
+
+from pypebbles.runtime import Environment
 
 
 def environment_variable(name: str, *, default: str | None = None) -> str:
-    return field(default_factory=lambda: value_of_env(variable=name, default=default))
+    return Environment().inject(variable=name, default=default)
 
 
 def value_of_env(*, variable: str, default: str | None = None) -> str:
-    return os.environ[variable] if default is None else os.getenv(variable, default)
+    return Environment().value_of(variable=variable, default=default)
