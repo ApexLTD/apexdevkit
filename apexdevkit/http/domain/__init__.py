7c71c4d315de9c95e88965e73a6d395990dd2501
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-11 16:26:22 +0400

    Rearrange imports

--- apexdevkit/http/domain/__init__.py
+++ apexdevkit/http/domain/__init__.py
@@ -1,7 +1,7 @@
-from apexdevkit.http.domain.method import HttpMethod
-from apexdevkit.http.domain.request import HttpRequest
-from apexdevkit.http.domain.response import HttpResponse
-from apexdevkit.http.domain.transport import HttpTransport
+from .method import HttpMethod
+from .request import HttpRequest
+from .response import HttpResponse
+from .transport import HttpTransport
 
 __all__ = [
     "HttpMethod",
