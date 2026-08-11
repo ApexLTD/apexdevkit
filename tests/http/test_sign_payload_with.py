2fed4840c3f706aaa1ce7721e1b1fdb0a622ee50
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-12 00:41:26 +0400

    Rewrite using transport

--- tests/http/test_sign_payload_with.py
+++ tests/http/test_sign_payload_with.py
@@ -4,7 +4,8 @@ import pytest
 from pypebbles import JsonDict
 from pypebbles.runtime import Environment
 
-from apexdevkit.http import FluentHttp, SignPayloadWith
+from apexdevkit.http import SignPayloadWith
+from apexdevkit.http.domain import HttpRequest, HttpTransport
 from apexdevkit.http.httpx.client import HttpxBuilder
 from apexdevkit.security import Signature
 from tests.http.echo import Echo
@@ -22,19 +23,23 @@ class FakeAuthority:
 
 
 @pytest.fixture
-def http() -> FluentHttp:
+def transport() -> HttpTransport:
     return (
         HttpxBuilder()
         .with_url(Environment().value_of("ECHO_SERVER"))
         .before_request(SignPayloadWith(FakeAuthority()))
-        .build()
+        .transport()
     )
 
 
 @pytest.mark.vcr
-def test_should_hook_post_method(http: FluentHttp) -> None:
-    payload = JsonDict().with_a(body="content")
-
-    echo = Echo(http.with_json(payload).on_endpoint("post").post().json())
-
-    assert echo.header(FakeAuthority.HEADER) == '{"body":"content"}'
+def test_should_hook_post_method(transport: HttpTransport) -> None:
+    (
+        HttpRequest()
+        .with_endpoint("post")
+        .with_json(value=JsonDict().with_a(body="content"))
+        .using(transport)
+        .post()
+        .load(Echo)
+        .assert_header(name=FakeAuthority.HEADER, value='{"body":"content"}')
+    )
