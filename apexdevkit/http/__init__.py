507e9adc4c61a8a8eae1b1095bf042fe2efb917d
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-08-12 00:44:57 +0400

    Remove redundant code

--- apexdevkit/http/__init__.py
+++ apexdevkit/http/__init__.py
@@ -1,11 +1,8 @@
-from apexdevkit.http.domain.method import HttpMethod
-
-from .fluent import FluentHttp
+from .domain import HttpMethod
 from .httpx import SignPayloadWith
 from .url import HttpUrl
 
 __all__ = [
-    "FluentHttp",
     "HttpMethod",
     "SignPayloadWith",
     "HttpUrl",
