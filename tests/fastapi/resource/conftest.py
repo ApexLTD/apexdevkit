4e5c38dea907208336688a2b92e8362bfe20dbc9
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2026-09-14 17:44:24 +0400

    Remove unused fixtures

--- tests/fastapi/resource/conftest.py
+++ tests/fastapi/resource/conftest.py
@@ -1,30 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
-from pypebbles.http import HttpRequest, HttpTransport
+from pypebbles.http import HttpTransport
 from pypebbles.http.drivers import Httpx
 
 from apexdevkit.fastapi import RestfulServiceBuilder
-from apexdevkit.fastapi.name import RestfulName
-from tests.fastapi.rest import RestCollection, RestTransport
 from tests.fastapi.sample_api import setup
-
-
-@pytest.fixture
-def resource(transport: HttpTransport) -> RestCollection:
-    return RestCollection(
-        name=RestfulName("market-apple"),
-        transport=RestTransport(transport),
-        request=HttpRequest().with_endpoint("market-apples"),
-    )
-
-
-@pytest.fixture
-def read_many_resource(transport: HttpTransport) -> RestCollection:
-    return RestCollection(
-        name=RestfulName("apple"),
-        transport=RestTransport(transport),
-        request=HttpRequest().with_endpoint("apples"),
-    )
 
 
 @pytest.fixture
